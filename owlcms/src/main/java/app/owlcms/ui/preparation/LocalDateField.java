acaf759ae9aba05a49571c0ef209d22bfd3956db
jflamy <jf@jflamy.dev>
2019-03-19 22:33:03 -0400

    General pattern for parsing textual content

--- owlcms/src/main/java/app/owlcms/ui/preparation/LocalDateField.java
+++ owlcms/src/main/java/app/owlcms/ui/preparation/LocalDateField.java
@@ -1,3 +1,11 @@
+/***
+ * Copyright (c) 2018-2019 Jean-François Lamy
+ * 
+ * This software is licensed under the the Apache 2.0 License amended with the
+ * Commons Clause.
+ * License text at https://github.com/jflamy/owlcms4/master/License
+ * See https://redislabs.com/wp-content/uploads/2018/10/Commons-Clause-White-Paper.pdf
+ */
 package app.owlcms.ui.preparation;
 
 import java.time.LocalDate;
@@ -6,69 +14,78 @@ import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
 import java.util.Locale;
 
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.textfield.TextField;
+import org.slf4j.LoggerFactory;
+
 import com.vaadin.flow.data.binder.Result;
 import com.vaadin.flow.data.binder.Validator;
 import com.vaadin.flow.data.binder.ValueContext;
 import com.vaadin.flow.data.converter.Converter;
 
-import app.owlcms.ui.crudui.Bindable;
-import app.owlcms.ui.crudui.OwlcmsCrudFormFactory;
+import app.owlcms.utils.WrappedTextField;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 /**
- * Conversion and validation methods for Body Weight
- * 
- * {@link OwlcmsCrudFormFactory} binds all instances of {@link Bindable} in this sequence
- *
- * <code><pre>
- * binder.forField(bwField);  // bwField is a subclass of TextField
- * binder.withConverter(bwField.getConverter());
- * binder.withValidator(bwField.getValidator());
- * binder.bind(property);  // property is of type T
- * </pre></code>
+ * Implement a LocalDate field with converter and validators.
  * 
  * @author Jean-François Lamy
  *
  */
 @SuppressWarnings("serial")
-public class LocalDateField extends TextField implements Bindable<LocalDate> {
+public class LocalDateField extends WrappedTextField<LocalDate> {
+	
+	private final Logger logger = (Logger)LoggerFactory.getLogger(LocalDateField.class);
+	@Override
+	protected void initLoggers() {
+		logger.setLevel(Level.DEBUG);
+	}
 
 	private static final DateTimeFormatter ISO_LOCAL_DATE = DateTimeFormatter.ISO_LOCAL_DATE;
+	
+	public LocalDateField() {
+		this(null, null);
+	}
+	
+	public LocalDateField(String label, LocalDate defaultValue) {
+		super(defaultValue);
+		initLoggers();
+		setLabel(label);
+		getWrappedTextField().addValueChangeListener(
+			event -> {
+				String presentationValue = event.getValue();
+				doConvertToModel(presentationValue, true);
+			});
+	}
 
 	@Override
 	public Converter<String, LocalDate> getConverter() {
-		Locale locale = UI.getCurrent().getLocale();
 		
 		return new Converter<String,LocalDate>() {
 			
 			@Override
 			public String convertToPresentation(LocalDate value, ValueContext context) {
-				if (value == null) value = LocalDate.now();			
-				return ISO_LOCAL_DATE.format(value);
+				Locale locale = context.getLocale().orElse(Locale.ENGLISH);
+				if (value == null) value = LocalDate.now();
+				return ISO_LOCAL_DATE.withLocale(locale).format(value);
 			}
 
-			/**
-			 * The full value must be parsed -- by default parsing stops at first error.
-			 * 
-			 * @see com.vaadin.flow.data.converter.StringToDoubleConverter#convertToModel(java.lang.String, com.vaadin.flow.data.binder.ValueContext)
-			 */
 			@Override
 			public Result<LocalDate> convertToModel(String value, ValueContext context) {
-				return tryFormatter(value, context, ISO_LOCAL_DATE);
+				return parser1(value, context, ISO_LOCAL_DATE);
 			}
 
-			protected Result<LocalDate> tryFormatter(String value, ValueContext context, DateTimeFormatter formatter) {
+			protected Result<LocalDate> parser1(String value, ValueContext context, DateTimeFormatter formatter) {
+				Locale locale = context.getLocale().orElse(Locale.ENGLISH);
 				LocalDate parse;
 				try {
 					parse = LocalDate.parse(value, ISO_LOCAL_DATE);
 					return Result.ok(parse);
 				} catch (DateTimeParseException e) {
-					return Result.error(this.getErrorMessage(context));
+					return Result.error(this.getErrorMessage(locale));
 				}
 			}
 
-			private String getErrorMessage(ValueContext context) {
+			private String getErrorMessage(Locale locale) {
 				return "Date must be in international format YYYY-MM-DD  (2000-12-31 for "
 						+ DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(locale)
 							.format(LocalDate.of(2000, 12, 31))
@@ -76,10 +93,14 @@ public class LocalDateField extends TextField implements Bindable<LocalDate> {
 			}
 		};
 	}
+	
+	public Validator<LocalDate> getNotFutureValidator() {
+		return Validator.from(ld -> (ld.compareTo(LocalDate.now()) < 0), "cannot be in the future");
+	}
 
 	@Override
-	public Validator<LocalDate> getValidator() {
-		return Validator.from(ld -> (ld.compareTo(LocalDate.now()) < 0), "cannot be in the future");
+	protected void logConversionError(String e) {
+		logger.error(e);
 	}
 }
 
---
+++ owlcms/src/main/java/app/owlcms/utils/WrappedTextField.java
@@ -0,0 +1,85 @@
+package app.owlcms.utils;
+
+
+import com.vaadin.flow.component.AbstractCompositeField;
+import com.vaadin.flow.component.HasValidation;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Result;
+import com.vaadin.flow.data.binder.ValueContext;
+import com.vaadin.flow.data.converter.Converter;
+
+@SuppressWarnings("serial")
+public abstract class WrappedTextField<T> extends AbstractCompositeField<TextField, WrappedTextField<T>, T> implements HasValidation {
+	
+	public WrappedTextField() {
+		this(null, null);
+	}
+
+	public WrappedTextField(String label, T defaultValue) {
+		super(defaultValue);
+		initLoggers();
+		setLabel(label);
+		getWrappedTextField().addValueChangeListener(
+			event -> {
+				String presentationValue = event.getValue();
+				doConvertToModel(presentationValue, true);
+			});
+	}
+
+	public WrappedTextField(T defaultValue) {
+		super(defaultValue);
+	}
+	
+	public TextField getWrappedTextField() {
+		return getContent();
+	}
+
+	@Override
+	public void setErrorMessage(String e) {
+		getWrappedTextField().setErrorMessage(e);
+	}
+
+	@Override
+	public String getErrorMessage() {
+		return getWrappedTextField().getErrorMessage();
+	}
+
+	@Override
+	public void setInvalid(boolean invalid) {
+		getWrappedTextField().setInvalid(invalid);
+	}
+
+	@Override
+	public boolean isInvalid() {
+		return getWrappedTextField().isInvalid();
+	}
+
+	public void setLabel(String label) {
+		getWrappedTextField().setLabel(label);
+	}
+
+	abstract public Converter<String, T> getConverter();
+	
+	protected void doConvertToModel(String presentationValue, boolean fromClient) {
+		Result<T> modelValue = getConverter().convertToModel(presentationValue, new ValueContext(this.getLocale()));
+		modelValue.ifOk(v -> super.setModelValue(v, fromClient));
+		modelValue.ifError(e -> {
+			setErrorMessage(e);
+			logConversionError(e);
+		});
+	}
+	
+	@Override
+	protected void setPresentationValue(T value) {
+		// there is only one TextField inside us, so we can manipulate directly
+        if (value == null)
+            getWrappedTextField().clear();
+        else
+            getWrappedTextField().setValue(getConverter().convertToPresentation(value, new ValueContext(UI.getCurrent().getLocale())));
+	}
+
+	protected abstract void logConversionError(String e);
+
+	protected abstract void initLoggers();
+}
