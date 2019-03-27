cab766aaa21550ca4bab388be6925e476c4a2636
jflamy <jf@jflamy.dev>
2019-03-26 22:53:21 -0400

    Open display in new tab

--- owlcms/src/main/java/app/owlcms/ui/displays/DisplayNavigationContent.java
+++ owlcms/src/main/java/app/owlcms/ui/displays/DisplayNavigationContent.java
@@ -8,33 +8,52 @@
  */
 package app.owlcms.ui.displays;
 
+import org.slf4j.LoggerFactory;
+
 import com.github.appreciated.layout.FlexibleGridLayout;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteConfiguration;
+import com.vaadin.flow.server.VaadinServletRequest;
 
 import app.owlcms.displays.attemptboard.AttemptBoard;
 import app.owlcms.displays.results.ResultsBoard;
 import app.owlcms.ui.home.ContentWrapping;
 import app.owlcms.ui.home.MainNavigationContent;
 import app.owlcms.ui.preparation.CategoryContent;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 /**
  * The Class DisplayNavigationContent.
  */
 @SuppressWarnings("serial")
-@Route(value = "app.owlcms.ui.displays", layout = DisplayNavigationLayout.class)
+@Route(value = "displays", layout = DisplayNavigationLayout.class)
 public class DisplayNavigationContent extends VerticalLayout
 		implements ContentWrapping {
+	
+	Logger logger = (Logger)LoggerFactory.getLogger(DisplayNavigationContent.class);
+	{ logger.setLevel(Level.DEBUG); }
 
 	/**
 	 * Instantiates a new display navigation content.
 	 */
 	public DisplayNavigationContent() {
+		VerticalLayout intro = new VerticalLayout();
+		intro.add(new Paragraph("Use the dropdown to select the platform where the display is located."));
+		intro.add(new Paragraph("Use one of the buttons below to open a display."));
+		
+		RouteConfiguration routeResolver = RouteConfiguration.forApplicationScope();
+		String attBoard = routeResolver.getUrl(AttemptBoard.class);
+		String attUrl = createURL(VaadinServletRequest.getCurrent(),attBoard);
+		logger.debug("url {}",attUrl);
+		
 		Button attempt = new Button("Attempt Board",
-				buttonClickEvent -> UI.getCurrent()
-					.navigate(AttemptBoard.class));
+				buttonClickEvent -> UI.getCurrent().getPage()
+					.executeJavaScript("window.open('"+attUrl+"','_blank')"));
 		Button results = new Button("Results Board",
 			buttonClickEvent -> UI.getCurrent()
 				.navigate(ResultsBoard.class));
@@ -58,9 +77,31 @@ public class DisplayNavigationContent extends VerticalLayout
 		referee.setEnabled(false);
 		jury.setEnabled(false);
 		plates.setEnabled(false);
-		
+
+		fillH(intro,this);
 		fillH(grid, this);
 
+	}
+	
+	protected static String createURL(VaadinServletRequest request, String resourcePath) {
+		int port = request.getServerPort();
+		StringBuilder result = new StringBuilder();
+		result.append(request.getScheme())
+			.append("://")
+			.append(request.getServerName());
+		if ((request.getScheme().equals("http") && port != 80)
+				|| (request.getScheme().equals("https") && port != 443)) {
+			result.append(':')
+				.append(port);
+		}
+		result.append(request.getContextPath());
+		if (resourcePath != null && resourcePath.length() > 0) {
+			if (!resourcePath.startsWith("/")) {
+				result.append("/");
+			}
+			result.append(resourcePath);
+		}
+		return result.toString();
 	}
 
 }
