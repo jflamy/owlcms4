f4dee8b192b65403b5f611e48075c0643152b9aa
jflamy <jf@jflamy.dev>
2019-04-16 20:34:32 -0400

    Switch to OSL 3.0 license

--- owlcms/src/main/java/app/owlcms/ui/group/UIEventProcessor.java
+++ owlcms/src/main/java/app/owlcms/ui/group/UIEventProcessor.java
@@ -1,3 +1,9 @@
+/***
+ * Copyright (c) 2009-2019 Jean-François Lamy
+ * 
+ * This software is licensed under the the Non-Profit Open Software License ("Non-Profit OSL") 3.0 
+ * License text at https://github.com/jflamy/owlcms4/master/License.txt
+ */
 package app.owlcms.ui.group;
 
 import java.util.Optional;
