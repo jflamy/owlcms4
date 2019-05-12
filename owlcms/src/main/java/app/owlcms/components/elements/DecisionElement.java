966fa3746fbecfc01f15ba7d72f472a6896a9dd9
jflamy <jf@jflamy.dev>
2019-05-11 22:06:18 -0400

    trace

--- owlcms/src/main/java/app/owlcms/components/elements/DecisionElement.java
+++ owlcms/src/main/java/app/owlcms/components/elements/DecisionElement.java
@@ -103,7 +103,7 @@ public class DecisionElement extends PolymerTemplate<DecisionElement.DecisionMod
 	@ClientCallable
 	public void masterShowDecisions(Boolean decision, Boolean ref1, Boolean ref2, Boolean ref3) {
 		Object origin = this.getOrigin();
-		logger.info("=== master {} decision={} ({} {} {})", origin, decision.getClass().getSimpleName(), ref1, ref2, ref3);
+		logger.debug("=== master {} decision={} ({} {} {})", origin, decision.getClass().getSimpleName(), ref1, ref2, ref3);
 		fopEventBus.post(new FOPEvent.RefereeDecision(origin, decision, ref1, ref2, ref3));
 	}
 
