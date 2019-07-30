53bd8cb16c0aa5ab5202b83fc7d68125cc888206
jflamy <jf@jflamy.dev>
2019-07-29 20:31:10 -0400

    #107 athlete cards now printed for all, even if weighed-in.

--- owlcms/src/main/java/app/owlcms/ui/lifting/WeighinLayout.java
+++ owlcms/src/main/java/app/owlcms/ui/lifting/WeighinLayout.java
@@ -154,7 +154,6 @@ public class WeighinLayout extends OwlcmsRouterLayout implements SafeEventBusReg
 		cardsButton = new Button(getTranslation("AthleteCards"),new Icon(VaadinIcon.DOWNLOAD_ALT)); //$NON-NLS-1$
 		cardsButton.addClickListener((e) -> {
 			cardsWriter.setGroup(group);
-			
 		});
 		cards.add(cardsButton);
 		cardsButton.setEnabled(true);
