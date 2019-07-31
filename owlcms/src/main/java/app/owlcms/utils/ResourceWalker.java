f09cb7dba3cfcc51958adf7451790da278fed72f
jflamy <jf@jflamy.dev>
2019-07-31 14:35:35 -0400

    get resource names recursively from jar or classpath

--- owlcms/src/main/java/app/owlcms/utils/ResourceWalker.java
+++ owlcms/src/main/java/app/owlcms/utils/ResourceWalker.java
@@ -1,3 +1,9 @@
+/***
+ * Copyright (c) 2009-2019 Jean-François Lamy
+ *
+ * Licensed under the Non-Profit Open Software License version 3.0  ("Non-Profit OSL" 3.0)
+ * License text at https://github.com/jflamy/owlcms4/blob/master/LICENSE.txt
+ */
 package app.owlcms.utils;
 
 import java.io.IOException;
@@ -23,14 +29,45 @@ import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Logger;
 
+/**
+ * Get resource paths recursively from jar or classpath and assign them a short
+ * display name for selection by user.
+ * 
+ * @author owlcms
+ *
+ */
 public class ResourceWalker {
+
+    public static void main(String[] args) {
+        try {
+            new ResourceWalker().walk("/templates/protocol", (filePath, rootPath) -> {
+                String displayName = relativeName(filePath, rootPath);
+                InputStream is;
+                try {
+                    is = Files.newInputStream(filePath);
+                    byte[] a = IOUtils.toByteArray(is);
+                    System.out.println(displayName + " " + filePath + " " + a.length + "bytes");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                return displayName;
+            });
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String relativeName(Path filePath, Path rootPath) {
+        return filePath.toString().substring(rootPath.toString().length() + 1);
+    }
 
     Logger logger = (Logger) LoggerFactory.getLogger(ResourceWalker.class);
 
     /**
      * Walk a resource tree and return the entries. The paths can be inside a jar or
-     * classpath folder. A function is called on the name in order to generate a display name.
-     * 
+     * classpath folder. A function is called on the name in order to generate a
+     * display name.
+     *
      * @param absoluteRoot a starting point (absolute resource name starts with a /)
      * @param generateName a function that takes the current path and the starting
      *                     path and returns a (unique) display name.
@@ -57,28 +94,5 @@ public class ResourceWalker {
             });
         }
         return processedNames;
-    }
-
-    private static String relativeName(Path filePath, Path rootPath) {
-        return filePath.toString().substring(rootPath.toString().length() + 1);
-    }
-
-    public static void main(String[] args) {
-        try {
-            new ResourceWalker().walk("/templates/protocol", (filePath, rootPath) -> {
-                String displayName = relativeName(filePath, rootPath);
-                InputStream is;
-                try {
-                    is = Files.newInputStream(filePath);
-                    byte[] a = IOUtils.toByteArray(is);
-                    System.out.println(displayName + " " + filePath + " " + a.length+ "bytes");
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                return displayName;
-            });
-        } catch (IOException | URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
