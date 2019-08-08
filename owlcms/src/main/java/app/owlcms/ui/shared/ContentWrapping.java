c5d32b7b472f698516a0d8c51342ebc8963b7d79
jflamy <jf@jflamy.dev>
2019-08-08 15:31:52 -0400

    Latest applayout with FlexBox

--- owlcms/src/main/java/app/owlcms/ui/shared/ContentWrapping.java
+++ owlcms/src/main/java/app/owlcms/ui/shared/ContentWrapping.java
@@ -7,22 +7,43 @@
 package app.owlcms.ui.shared;
 
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.orderedlayout.BoxSizing;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public interface ContentWrapping {
-
-    public default void fillHW(Component content, VerticalLayout vWrapper) {
-        vWrapper.setBoxSizing(BoxSizing.BORDER_BOX);
+	
+	public default void fillHW (Component content, VerticalLayout vWrapper) {
+		HorizontalLayout hWrapper = new HorizontalLayout(content);
+        hWrapper.setMargin(true);
+        hWrapper.setPadding(false);
+        hWrapper.setSpacing(false);
+        hWrapper.setFlexGrow(1, content);
+        //hWrapper.setSizeFull();
+        vWrapper.add(hWrapper);
+        vWrapper.setMargin(false);
+        vWrapper.setPadding(false);
+        vWrapper.setSpacing(false);
+        vWrapper.setAlignItems(Alignment.STRETCH);
+        vWrapper.setFlexGrow(1, hWrapper);
         vWrapper.setSizeFull();
-        vWrapper.add(content);
-    }
-
-    public default void fillH(Component content, VerticalLayout vWrapper) {
-        vWrapper.setBoxSizing(BoxSizing.BORDER_BOX);
-        vWrapper.setHeight(null);
-        vWrapper.setWidth("100%");
-        vWrapper.add(content);
-    }
+	}
+	
+	public default void fillH(Component content, VerticalLayout vWrapper) {
+		HorizontalLayout hWrapper = new HorizontalLayout(content);
+        hWrapper.setMargin(true);
+        hWrapper.setPadding(false);
+        hWrapper.setSpacing(false);
+        hWrapper.setFlexGrow(1, content);
+        hWrapper.getElement().getStyle().set("margin-bottom", "0"); //$NON-NLS-1$ //$NON-NLS-2$
+        //hWrapper.setSizeFull();
+        vWrapper.add(hWrapper);
+        vWrapper.setMargin(false);
+        vWrapper.setPadding(false);
+        vWrapper.setSpacing(false);
+        vWrapper.setAlignItems(Alignment.STRETCH);
+//        vWrapper.setFlexGrow(1, hWrapper);
+//        vWrapper.setSizeFull();
+	}
 
 }
