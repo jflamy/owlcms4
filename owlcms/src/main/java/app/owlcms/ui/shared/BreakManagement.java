e4a1027cf0f87bc824d1465db015454bc8140cc6
Jean-François Lamy <jf@jflamy.dev>
2019-11-25 11:10:09 -0500

    cleanup old tracing

--- owlcms/src/main/java/app/owlcms/ui/shared/BreakManagement.java
+++ owlcms/src/main/java/app/owlcms/ui/shared/BreakManagement.java
@@ -336,7 +336,7 @@ public class BreakManagement extends VerticalLayout implements SafeEventBusRegis
 
             if (bType != null && (bType == BreakType.JURY || bType == BreakType.TECHNICAL
                     || bType == BreakType.DURING_INTRODUCTION)) {
-                logger.warn("starting break from radiobutton setvalue {}", bType);
+                logger.debug("starting break from radiobutton setvalue {}", bType);
                 startIndefiniteBreakImmediately(bType);
             } else {
                 setBreakTimeRemaining(ct.getValue());
@@ -396,7 +396,7 @@ public class BreakManagement extends VerticalLayout implements SafeEventBusRegis
                 value = (value == null ? DEFAULT_DURATION : value);
 //                if (value.compareTo(Duration.ofMinutes(50)) > 0) {
 //                    // defensive; should no longer be required
-//                    logger.warn("setting to absurd duration {}",value);
+//                    logger.debug("setting to absurd duration {}",value);
 //                    value = DEFAULT_DURATION;
 //                    durationField.setValue(value);
 //                }
