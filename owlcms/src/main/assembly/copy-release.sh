59d8e4a9f3b171c72f0a11563e5fda1320b79c4e
Jean-François Lamy <jf@jflamy.dev>
2019-11-27 08:34:54 -0500

    Force tag update on rc release

--- owlcms/src/main/assembly/copy-release.sh
+++ owlcms/src/main/assembly/copy-release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 cd "${project.basedir}"
-git tag -a ${project.version} -m "${project.version}"
+git tag -a ${project.version} -m "${project.version}" -f
 cp target/owlcms_setup/owlcms_setup.exe ~/Dropbox/owlcms/owlcms_setup_${project.version}.exe
 cp target/owlcms.zip ~/Dropbox/owlcms/owlcms_${project.version}.zip
