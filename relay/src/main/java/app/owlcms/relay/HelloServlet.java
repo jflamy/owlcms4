8f3b2a039c9264a472af57200f45d9928819f306
Jean-François Lamy <jf@jflamy.dev>
2020-01-07 17:10:10 -0500

    simultaneous vaadin atmosphere and REST server

--- relay/src/main/java/app/owlcms/relay/HelloServlet.java
+++ relay/src/main/java/app/owlcms/relay/HelloServlet.java
@@ -15,6 +15,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        
         PrintWriter osw = new PrintWriter(resp.getOutputStream());
         osw.print("Hello!");
         osw.flush();
