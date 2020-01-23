7f98e4898a72f010aca87e9f6daa0773c11286bf
Jean-FranÃ§ois Lamy <jf@jflamy.dev>
2020-01-22 21:55:25 -0500

    deleted

--- owlcms@tmp/durable-11ae55fc/jenkins-wrap.bat
+++
@@ -1,3 +0,0 @@
-@echo off 
-cmd /c ""C:\Program Files (x86)\Jenkins\workspace\build_owlcms_develop\owlcms@tmp\durable-11ae55fc\jenkins-main.bat"" > "C:\Program Files (x86)\Jenkins\workspace\build_owlcms_develop\owlcms@tmp\durable-11ae55fc\jenkins-log.txt" 2>&1
-echo %ERRORLEVEL% > "C:\Program Files (x86)\Jenkins\workspace\build_owlcms_develop\owlcms@tmp\durable-11ae55fc\jenkins-result.txt"
