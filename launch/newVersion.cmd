14f692799bf7414ef92d85b57f44ee4a4a6a956d
Jean-François Lamy <jf@jflamy.dev>
2020-01-27 20:47:46 -0500

    4.4.1-beta2

--- launch/newVersion.cmd
+++ launch/newVersion.cmd
@@ -9,5 +9,6 @@ git commit -a -m "%1%
 git pull
 git push
 echo Done.
-curl -d "VERSION=%1%" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://owlcms:owlcms@localhost:8080/job/build%%20owlcms/job/develop/buildWithParameters
+curl -X POST http://owlcms:owlcm@localhost:8080/job/test/buildWithParameter --data-urlencode json='{"parameter": [{"name":"VERSION", "value":"%1%"}]}'
+
 rem curl -X POST http://owlcms:owlcms@localhost:8080/job/build%%20owlcms/job/develop/build
