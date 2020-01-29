19576208e8141a0977103c734234f0c1ce6eb68a
Jean-François Lamy <jf@jflamy.dev>
2020-01-28 21:21:21 -0500

    4.4.1-beta6

--- launch/newRelease.cmd
+++ launch/newRelease.cmd
@@ -8,4 +8,5 @@ git commit -a -m "%1%
 git pull
 git push
 echo Done.
+echo %REPO_OWNER%
 echo curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=%1%&REPO_OWNER=%REPO_OWNER%&O_REPO_NAME=%O_REPO_NAME%&P_REPO_NAME=%P_REPO_NAME%"
