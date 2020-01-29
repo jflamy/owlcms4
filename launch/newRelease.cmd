065f465345602e3a88bdad096c2e338f9aa5ad41
Jean-François Lamy <jf@jflamy.dev>
2020-01-28 21:03:21 -0500

    4.4.1-beta6

--- launch/newRelease.cmd
+++ launch/newRelease.cmd
@@ -1,4 +1,3 @@
-echo %1%
 call mvn "versions:set" "versions:commit" "-DnewVersion=%1%" "-DoldVersion=*" "-DgroupId=*" "-DartifactId=*" 
 cd owlcms-publicresults
 git commit -a -m "%1%
@@ -9,4 +8,4 @@ git commit -a -m "%1%
 git pull
 git push
 echo Done.
-curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=%1%&REPO_OWNER=owlcms&O_REPO_NAME=owlcms4&P_REPO_NAME=owlcms-publicresults"
+curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=%1%&REPO_OWNER=%REPO_OWNER%&O_REPO_NAME=%O_REPO_NAME%&P_REPO_NAME=%P_REPO_NAME%"
