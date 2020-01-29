e2e01235bff28a39172b179c11beb3e26ba289db
Jean-François Lamy <jf@jflamy.dev>
2020-01-28 21:47:00 -0500

    positional parameters

--- launch/newRelease.cmd
+++ launch/newRelease.cmd
@@ -8,5 +8,4 @@ git commit -a -m "%1%
 git pull
 git push
 echo Done.
-set VERSION=%1%
-wsl curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=${VERSION}&REPO_OWNER=${REPO_OWNER}&O_REPO_NAME=${O_REPO_NAME}&P_REPO_NAME=${P_REPO_NAME}&PRERELEASE=${PRERELEASE}"
+curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=%1%&REPO_OWNER=%2%&O_REPO_NAME=%3%&P_REPO_NAME=%4%&PRERELEASE=%5%"
