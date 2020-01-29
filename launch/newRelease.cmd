00183cd11806ac9613941a7b91352ef9824ca826
Jean-François Lamy <jf@jflamy.dev>
2020-01-28 21:39:23 -0500

    4.4.1-beta7

--- launch/newRelease.cmd
+++ launch/newRelease.cmd
@@ -9,4 +9,4 @@ git pull
 git push
 echo Done.
 set VERSION=%1%
-wsl curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=${VERSION}&REPO_OWNER=${REPO_OWNER}&O_REPO_NAME=${O_REPO_NAME}&P_REPO_NAME=${P_REPO_NAME}&PRERELEASE=${PRERELEASE}
+wsl curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=${VERSION}&REPO_OWNER=${REPO_OWNER}&O_REPO_NAME=${O_REPO_NAME}&P_REPO_NAME=${P_REPO_NAME}&PRERELEASE=${PRERELEASE}"
