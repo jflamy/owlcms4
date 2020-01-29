5cb75b4063c2b9f5b7c4c74f82cd1763d385c8bd
Jean-François Lamy <jf@jflamy.dev>
2020-01-29 11:23:16 -0500

    4.4.1

--- launch/newRelease.cmd
+++ launch/newRelease.cmd
@@ -7,5 +7,5 @@ cd ..
 git commit -a -m "%1"
 git pull
 git push
-echo Launching Build VERSION=%1 REPO_OWNER=%2 O_REPO_NAME=%3 P_REPO_NAME=%4 PRERELEASE=%5
-curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=%1&REPO_OWNER=%2&O_REPO_NAME=%3&P_REPO_NAME=%4&PRERELEASE=%5"
+echo Launching Master Build VERSION=%1 REPO_OWNER=%2 O_REPO_NAME=%3 P_REPO_NAME=%4 PRERELEASE=%5
+curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/master/buildWithParameters?VERSION=%1&REPO_OWNER=%2&O_REPO_NAME=%3&P_REPO_NAME=%4&PRERELEASE=%5"
