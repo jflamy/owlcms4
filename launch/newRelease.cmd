3ac1b28aa286e6322311c780f18989dee76b34db
Jean-François Lamy <jf@jflamy.dev>
2020-01-28 20:34:58 -0500

    4.4.1-beta4

--- launch/newRelease.cmd
+++ launch/newRelease.cmd
@@ -9,4 +9,4 @@ git commit -a -m "%1%
 git pull
 git push
 echo Done.
-curl -X POST http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=%1%\&REPO_OWNER=owlcms\&O_REPO_NAME=owlcms4\&P_REPO_NAME=owlcms-publicresults
+curl -X POST 'http://owlcms:owlcms@localhost:8080/job/owlcms/job/develop/buildWithParameters?VERSION=%1%&REPO_OWNER=owlcms&O_REPO_NAME=owlcms4&P_REPO_NAME=owlcms-publicresults'
