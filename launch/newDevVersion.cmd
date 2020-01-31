4f61708d086bee28077028b968c48ac46e75116a
Jean-François Lamy <jf@jflamy.dev>
2020-01-31 15:27:12 -0500

    4.4.2-rc1

--- launch/newDevVersion.cmd
+++ launch/newDevVersion.cmd
@@ -1,11 +1,13 @@
 echo %1%
+cd publicresults-heroku
+git pull
+cd ..
+git pull
 call mvn "versions:set" "versions:commit" "-DnewVersion=%1%" "-DoldVersion=*" "-DgroupId=*" "-DartifactId=*" 
 cd publicresults-heroku
 git commit -a -m "%1%
-git pull
 git push
 cd ..
 git commit -a -m "%1%
-git pull
 git push
 echo Done.
