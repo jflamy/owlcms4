81d14fc4a7e2dd4e84648d3c0f947f430784d589
Jean-François Lamy <jf@jflamy.dev>
2020-02-03 15:09:46 -0500

    new submodule

--- launch/newRelease.cmd
+++ launch/newRelease.cmd
@@ -8,4 +8,4 @@ git commit -a -m "%1"
 git pull
 git push
 echo Launching Build VERSION=%1 REPO_OWNER=%2 O_REPO_NAME=%3 P_REPO_NAME=%4 PRERELEASE=%5 BRANCH=%6
-curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/%6/buildWithParameters?VERSION=%1&REPO_OWNER=%2&O_REPO_NAME=%3&P_REPO_NAME=%4&PRERELEASE=%5&BRANCH=%6"
+curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/%6/buildWithParameters?VERSION=%1&REPO_OWNER=%2&O_REPO_NAME=%3&P_REPO_NAME=%4&PRERELEASE=%5&BRANCH=%6&H_REPO_NAME=%7"
