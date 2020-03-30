c4395d9c83c414e64114a4d9151d8c75bdc493bc
Jean-François Lamy <jf@jflamy.dev>
2020-03-29 22:42:20 -0400

    4.6.2-rc2

--- launch/newRelease.cmd
+++ launch/newRelease.cmd
@@ -1,3 +1,4 @@
+LOCAL_TOKEN=11b65aee741aa15d84be95b8d4fac9e27a
 call mvn "versions:set" "versions:commit" "-DnewVersion=%1" "-DoldVersion=*" "-DgroupId=*" "-DartifactId=*" 
 cd publicresults-heroku
 git commit -a -m "%1"
@@ -14,4 +15,4 @@ git pull
 git push
 git tag "build_%1"
 echo Launching Build VERSION=%1 REPO_OWNER=%2 O_REPO_NAME=%3 P_REPO_NAME=%4 PRERELEASE=%5 BRANCH=%6
-curl -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/%6/buildWithParameters?VERSION=%1&REPO_OWNER=%2&O_REPO_NAME=%3&P_REPO_NAME=%4&PRERELEASE=%5&BRANCH=%6&H_REPO_NAME=%7"
+curl --user owlcms:%LOCAL_TOKEN% -X POST "http://owlcms:owlcms@localhost:8080/job/owlcms/job/%6/buildWithParameters?VERSION=%1&REPO_OWNER=%2&O_REPO_NAME=%3&P_REPO_NAME=%4&PRERELEASE=%5&BRANCH=%6&H_REPO_NAME=%7"
