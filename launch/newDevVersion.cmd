f399f20b59b6da6d1e98abfd7d7a16c4e8abb0fc
Jean-François Lamy <jf@jflamy.dev>
2020-05-23 21:17:28 -0400

    4.7.5-beta6

--- launch/newDevVersion.cmd
+++ launch/newDevVersion.cmd
@@ -1,4 +1,4 @@
-call mvn "versions:set" "version:commit" "-DnewVersion=%1" "-DoldVersion=4.7.*" "-DgroupId=*" "-DartifactId=*"
+call mvn "versions:set" "version:commit" "-DnewVersion=%1" "-DoldVersion=4.**" "-DgroupId=*" "-DartifactId=*"
 call mvn "versions:commit"
 cd publicresults-heroku
 git commit -a -m "%1"
