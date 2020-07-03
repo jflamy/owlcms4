4fec14b3f131eef1b7dc31d8af9be9909845cf83
Jean-François Lamy <jf@jflamy.dev>
2020-07-03 12:09:07 -0400

    remove warning

--- owlcms/src/main/java/app/owlcms/ui/shared/DownloadButtonFactory.java
+++ owlcms/src/main/java/app/owlcms/ui/shared/DownloadButtonFactory.java
@@ -8,7 +8,6 @@ package app.owlcms.ui.shared;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 
 import org.slf4j.LoggerFactory;
 
