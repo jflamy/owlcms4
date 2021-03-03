5c27011522b6569c9521fad5963d81ca6e574011
Jean-François Lamy <jf@jflamy.dev>
2021-03-02 20:09:46 -0500

    k3d setups

--- owlcms-docker/src/main/assembly/k8s/build_setup.sh
+++ owlcms-docker/src/main/assembly/k8s/build_setup.sh
@@ -1,5 +1,6 @@
 #/bin/bash -wq
-(echo "# DockerDesktop setup"; cat base/nginx.yaml ; echo "---" ; kubectl kustomize overlays/kubesail-dd ) > dd_setup.yaml
-(echo "# k3s setup "; cat base/nginx.yaml ; echo "---" ; kubectl kustomize overlays/k3s ) > k3s_setup.yaml
-(echo "# k3s setup "; kubectl kustomize overlays/k3d ) > k3d_setup.yaml
-(echo "# k3d nocert setup "; kubectl kustomize overlays/local-nocerts ) > k3d_nocert.yaml
+(echo "# DockerDesktop setup"; kubectl kustomize overlays/dd ) > dd_setup.yaml
+(echo "# k3s setup "; kubectl kustomize overlays/k3s ) > k3s_setup.yaml
+(echo "# k3d setup "; kubectl kustomize overlays/k3d ) > k3d_setup.yaml
+(echo "# k3d nocert setup "; kubectl kustomize overlays/k3d-nocert ) > k3d_nocert.yaml
+echo done.
