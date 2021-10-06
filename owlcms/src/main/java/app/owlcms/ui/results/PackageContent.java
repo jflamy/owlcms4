1beb41f0739680e57890fdb427c87a3bd8c2df9d
Jean-François Lamy <jf@jflamy.dev>
2021-10-06 16:54:03 -0400

    cleanup

--- owlcms/src/main/java/app/owlcms/ui/results/PackageContent.java
+++ owlcms/src/main/java/app/owlcms/ui/results/PackageContent.java
@@ -59,7 +59,6 @@ import app.owlcms.ui.crudui.OwlcmsGridLayout;
 import app.owlcms.ui.shared.AthleteCrudGrid;
 import app.owlcms.ui.shared.AthleteGridContent;
 import app.owlcms.ui.shared.AthleteGridLayout;
-import app.owlcms.utils.LoggerUtils;
 import app.owlcms.utils.ResourceWalker;
 import app.owlcms.utils.URLUtils;
 import ch.qos.logback.classic.Level;
