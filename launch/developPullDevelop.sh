63cef3af4bb855cae67b7b44e065d0df3ec439f2
Jean-François Lamy <jf@jflamy.dev>
2021-11-21 10:02:26 -0500

    simplying merge.

--- launch/developPullDevelop.sh
+++ launch/developPullDevelop.sh
@@ -1,17 +1,8 @@
 git pull
-git submodule update --init --recursive --remote
-cd publicresults-heroku
-git checkout develop
-git pull
-cd ..
-cd owlcms-heroku
-git checkout develop
-git pull
-cd ..
-git checkout develop
-git pull
-git add .
-git commit -a -m "sync submodules [skip ci]"
+git submodule update --init --recursive --remote --merge
+git commit -m "sync submodules [skip ci]" publicresults-heroku
+git commit -m "sync submodules [skip ci]" owlcms-heroku
 git push
 echo Done. synced develop submodules.
+pause
 
