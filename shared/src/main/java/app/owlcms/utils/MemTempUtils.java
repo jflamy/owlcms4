016894588065e52c3884e79b3055daec601fc799
Jean-François Lamy <jf@jflamy.dev>
2021-12-07 22:01:01 -0500

    in-memory file system for temp files

--- shared/src/main/java/app/owlcms/utils/MemTempUtils.java
+++ shared/src/main/java/app/owlcms/utils/MemTempUtils.java
@@ -1,30 +1,34 @@
 package app.owlcms.utils;
 
 import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+
 public class MemTempUtils {
 
-//    private static FileSystem memFs = Jimfs.newFileSystem(Configuration.unix());
-//
-//    private static Path tempDir =  memFs.getRootDirectories().iterator().next();
+    private static FileSystem memFs = Jimfs.newFileSystem(Configuration.unix());
+
+    private static Path tempDir =  memFs.getRootDirectories().iterator().next();
 
     public static Path createTempDirectory() throws IOException {
-        //return Files.createTempDirectory(tempDir, Long.toString(System.currentTimeMillis()));
-        return Files.createTempDirectory(Long.toString(System.currentTimeMillis()));
+        return Files.createTempDirectory(tempDir, Long.toString(System.currentTimeMillis()));
+        //return Files.createTempDirectory(Long.toString(System.currentTimeMillis()));
     }
 
     public static Path createTempDirectory(String name) throws IOException {
-        //return Files.createTempDirectory(tempDir, name);
-        return Files.createTempDirectory(name);
+        return Files.createTempDirectory(tempDir, name);
+        //return Files.createTempDirectory(name);
     }
 
 
     public static Path createTempFile(String prefix, String suffix) {
         try {
-            //return Files.createTempFile(tempDir, prefix, suffix);
-            return Files.createTempFile(prefix, suffix);
+            return Files.createTempFile(tempDir, prefix, suffix);
+            //return Files.createTempFile(prefix, suffix);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -32,9 +36,9 @@ public class MemTempUtils {
     
     public static Path createTempFile(Path parentDir, String prefix, String suffix) {
         try {
-            //Path memDir = tempDir.resolve(parentDir);
-            //return Files.createTempFile(memDir, prefix, suffix);
-            return Files.createTempFile(parentDir, prefix, suffix);
+            Path memDir = tempDir.resolve(parentDir);
+            return Files.createTempFile(memDir, prefix, suffix);
+            //return Files.createTempFile(parentDir, prefix, suffix);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
