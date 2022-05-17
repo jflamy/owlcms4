7785fb390a45b480bb1da8320e6e15ee367ed1d2
Jean-François Lamy <jf@jflamy.dev>
2022-05-17 18:30:27 -0400

    results with no leaders should not show leaders

--- owlcms/src/main/java/app/owlcms/displays/scoreboard/ResultsNoLeaders.java
+++ owlcms/src/main/java/app/owlcms/displays/scoreboard/ResultsNoLeaders.java
@@ -7,6 +7,7 @@
 package app.owlcms.displays.scoreboard;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
@@ -15,6 +16,7 @@ import com.vaadin.flow.theme.lumo.Lumo;
 
 import app.owlcms.init.OwlcmsFactory;
 import app.owlcms.init.OwlcmsSession;
+import elemental.json.Json;
 
 /**
  * Class Scoreboard
@@ -38,7 +40,6 @@ public class ResultsNoLeaders extends Results {
         OwlcmsFactory.waitDBInitialized();
         timer.setOrigin(this);
         setDarkMode(true);
-        this.getElement().setProperty("leadersDisplay", "display: none;");
     }
 
     @Override
@@ -46,4 +47,11 @@ public class ResultsNoLeaders extends Results {
         return getTranslation("Scoreboard") + OwlcmsSession.getFopNameIfMultiple();
     }
 
+    @Override
+    protected void computeLeaders() {
+        UI.getCurrent().access(() -> {
+            this.getElement().setPropertyJson("leaders", Json.createNull());
+            this.getElement().setProperty("leaderLines", 1); // must be > 0
+        });
+    }
 }
