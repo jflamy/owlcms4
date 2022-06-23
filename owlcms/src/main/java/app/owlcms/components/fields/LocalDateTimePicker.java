40c5c6a9a148b4b6adf16fa3f09a750ee4f211f8
Jean-François Lamy <jf@jflamy.dev>
2022-06-23 09:43:11 -0400

    Use 24h time in English except for old-fashioned countries (#503)

--- owlcms/src/main/java/app/owlcms/components/fields/LocalDateTimePicker.java
+++ owlcms/src/main/java/app/owlcms/components/fields/LocalDateTimePicker.java
@@ -9,20 +9,44 @@ package app.owlcms.components.fields;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Locale;
+
+import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.component.customfield.CustomField;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.timepicker.TimePicker;
 
+import ch.qos.logback.classic.Logger;
+
 @SuppressWarnings("serial")
 public class LocalDateTimePicker extends CustomField<LocalDateTime> {
+    
+    Logger logger = (Logger) LoggerFactory.getLogger(LocalDateTimePicker.class);
 
     private final DatePicker datePicker = new DatePicker();
     private final TimePicker timePicker = new TimePicker();
+    
+    // override time format.  Sorted list of old-style countries. Canada does both, so excluded.
+    public final static String[] AM_PM_COUNTRIES = {"AU", "GB", "IN", "NZ", "PH", "US", "ZA"};
 
     public LocalDateTimePicker() {
         timePicker.getStyle().set("margin-left", "1em");
+        Locale l = timePicker.getLocale();
+        timePicker.setLocale(fixAM_PM(l));
         add(datePicker, timePicker);
+    }
+
+    public static Locale fixAM_PM(Locale l) {
+        if (l.getLanguage() != null && l.getLanguage().contentEquals("en")) {
+            String country = l.getCountry();
+            if (l != null && Arrays.binarySearch(AM_PM_COUNTRIES, country) <= 0) {
+                // use international format en_SE seems to work best.
+                return (new Locale("en", "SE"));
+            }
+        }
+        return l;
     }
 
     /**
