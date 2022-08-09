60e87a4d15219b7d17fdd4f78b9f6c26fa46dd5f
Jean-François Lamy <jf@jflamy.dev>
2022-08-09 09:23:23 -0400

    update merge scripts

--- launch/developPullDevelop.sh
+++ launch/developPullDevelop.sh
@@ -17,7 +17,7 @@ git fetch
 cd publicresults-heroku
 git checkout $LOCAL
 git fetch
-git merge origin/$REMOTE --no-ff -m "merge $REMOTE [skip ci]"
+git merge origin/$REMOTE --no-ff -m "merge origin/$REMOTE [skip ci]"
 git push origin $LOCAL
 cd ..
 git add publicresults-heroku
@@ -25,14 +25,13 @@ git add publicresults-heroku
 cd owlcms-heroku
 git checkout $LOCAL
 git fetch
-git merge origin/$REMOTE --no-ff -m "merge $REMOTE [skip ci]"
+git merge origin/$REMOTE --no-ff -m "merge origin/$REMOTE [skip ci]"
 git push origin $LOCAL
 cd ..
 git add owlcms-heroku
 
 git commit -m "sync submodules"
-
-git merge origin/$REMOTE --no-ff -m "merge $REMOTE [skip ci]"
+git merge origin/$REMOTE --no-ff -m "merge origin/$REMOTE [skip ci]"
 git push origin $LOCAL
 echo Done.  pulled $REMOTE into $LOCAL.
 
