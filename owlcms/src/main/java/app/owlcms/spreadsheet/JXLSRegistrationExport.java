09c95d2df55c12fcb54c3407bf5d29cc908f5365
Jean-François Lamy <jf@jflamy.dev>
2022-11-23 22:55:02 -0500

    fixed export order for groups

--- owlcms/src/main/java/app/owlcms/spreadsheet/JXLSRegistrationExport.java
+++ owlcms/src/main/java/app/owlcms/spreadsheet/JXLSRegistrationExport.java
@@ -8,9 +8,12 @@ package app.owlcms.spreadsheet;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.component.UI;
@@ -18,6 +21,7 @@ import com.vaadin.flow.component.UI;
 import app.owlcms.data.athlete.Athlete;
 import app.owlcms.data.athlete.AthleteRepository;
 import app.owlcms.data.athleteSort.AthleteSorter;
+import app.owlcms.data.group.GroupRepository;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 
@@ -51,6 +55,21 @@ public class JXLSRegistrationExport extends JXLSWorkbookStreamSource {
         List<Athlete> athletes = AthleteRepository.findAllByGroupAndWeighIn(null, null);
         return AthleteSorter
                 .registrationExportCopy(athletes);
+    }
+    
+    @Override
+    public HashMap<String, Object> getReportingBeans() {
+        HashMap<String, Object> beans = super.getReportingBeans();
+        // the purpose of allocating groups, better to sort by platform first.
+        beans.put("groups", GroupRepository.findAll().stream().sorted((a, b) -> {
+            int compare = ObjectUtils.compare(a.getPlatform(), b.getPlatform(), true);
+            if (compare != 0) {
+                return compare;
+            }
+            compare = ObjectUtils.compare(a.getWeighInTime(), b.getWeighInTime(), true);
+            return compare;
+        }).collect(Collectors.toList()));
+        return beans;
     }
 
 }
