73e800889e9ef3d6c762b488128206c3b4bed82e
Jean-François Lamy <jf@jflamy.dev>
2022-12-18 13:27:54 -0500

    navigation drawer done. titles need checking.

--- owlcms/src/main/java/app/owlcms/nui/shared/OwlcmsLayout.java
+++ owlcms/src/main/java/app/owlcms/nui/shared/OwlcmsLayout.java
@@ -7,7 +7,10 @@ import java.util.Locale;
 
 import org.slf4j.LoggerFactory;
 
+import com.flowingcode.vaadin.addons.ironicons.HardwareIcons;
 import com.flowingcode.vaadin.addons.ironicons.IronIcons;
+import com.flowingcode.vaadin.addons.ironicons.MapsIcons;
+import com.flowingcode.vaadin.addons.ironicons.PlacesIcons;
 import com.flowingcode.vaadin.addons.ironicons.SocialIcons;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasElement;
@@ -15,6 +18,7 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.IronIcon;
@@ -31,8 +35,11 @@ import com.vaadin.flow.router.RouterLink;
 
 import app.owlcms.i18n.Translator;
 import app.owlcms.init.OwlcmsSession;
+import app.owlcms.nui.displayselection.DisplayNavigationContent;
 import app.owlcms.nui.home.HomeNavigationContent;
+import app.owlcms.nui.lifting.LiftingNavigationContent;
 import app.owlcms.nui.preparation.PreparationNavigationContent;
+import app.owlcms.nui.results.ResultsNavigationContent;
 import app.owlcms.utils.LoggerUtils;
 import ch.qos.logback.classic.Logger;
 
@@ -46,13 +53,6 @@ public class OwlcmsLayout extends AppLayout {
     public static final String NONE = "m-0";
     Logger logger = (Logger) LoggerFactory.getLogger(OwlcmsLayout.class);
     private Label viewTitle;
-
-    String DOCUMENTATION = Translator.translate("Documentation_Menu");
-    String INFO = Translator.translate("About");
-    String PREFERENCES = Translator.translate("Preferences");
-    String RESULT_DOCUMENTS = Translator.translate("Results");
-    String RUN_LIFTING_GROUP = Translator.translate("RunLiftingGroup");
-    String START_DISPLAYS = Translator.translate("StartDisplays");
 
     protected List<Component> navBarComponents;
 
@@ -206,7 +206,7 @@ public class OwlcmsLayout extends AppLayout {
 //        VerticalLayout createNavigation = createNavigation();
 //        createNavigation.setWidth("90%");
 //        Scroller scroller = new Scroller(createNavigation);
-        
+
         Tabs tabs = getTabs();
 
         addToDrawer(tabs);
@@ -259,12 +259,52 @@ public class OwlcmsLayout extends AppLayout {
 
     private Tabs getTabs() {
         Tabs tabs = new Tabs();
+        String docOpener = "javascript:window.open('https://jflamy.github.io/owlcms4/#/index','_blank')";
         tabs.add(
-                createTab(IronIcons.HOME.create(), Translator.translate("Home"), HomeNavigationContent.class),
-                createTab(SocialIcons.GROUP_ADD.create(), Translator.translate("PrepareCompetition"), PreparationNavigationContent.class));
+                createTab(IronIcons.HOME.create(), 
+                        Translator.translate("Home"), 
+                        HomeNavigationContent.class),
+                createTab(SocialIcons.GROUP_ADD.create(), 
+                        Translator.translate("PrepareCompetition"),
+                        PreparationNavigationContent.class),
+                createTab(PlacesIcons.FITNESS_CENTER.create(), 
+                        Translator.translate("RunLiftingGroup"),
+                        LiftingNavigationContent.class),
+                createTab(HardwareIcons.DESKTOP_WINDOWS.create(), 
+                        Translator.translate("StartDisplays"),
+                        DisplayNavigationContent.class),
+                createTab(MapsIcons.LOCAL_PRINTSHOP.create(), 
+                        Translator.translate("Results"),
+                        ResultsNavigationContent.class),
+                createTab(IronIcons.HELP.create(), 
+                        Translator.translate("Documentation_Menu"),
+                        docOpener
+                        ),
+                createTab(IronIcons.INFO_OUTLINE.create(), 
+                        Translator.translate("About"),
+                        PreparationNavigationContent.class)
+//                createTab(IronIcons.SETTINGS.create(), 
+//                        Translator.translate("Preferences"),
+//                        PreparationNavigationContent.class)
+                );
 
+        Translator.translate("RunLiftingGroup");
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         return tabs;
+    }
+
+    private Tab createTab(IronIcon viewIcon, String viewName,
+            String docOpener) {
+        Anchor a = new Anchor();
+        a.setHref(docOpener);
+        a.add(viewIcon);
+        // copied from router-link
+        viewIcon.getElement().setAttribute("style", "padding: var(--lumo-space-xs); margin-inline-end: var(--lumo-space-m); box-sizing: border-box;");
+        a.add(new Span(viewName));
+        a.getElement().setAttribute("router-link", true);
+
+        return new Tab(a);
+
     }
 
     private Tab createTab(IronIcon viewIcon, String viewName, Class<? extends Component> viewClass) {
