5aeafbf6b4be661b0679ae8407e3cc8a1a07ec17
Jean-François Lamy <jf@jflamy.dev>
2022-12-24 09:43:18 -0500

    slow down hyperactive loading indicator

--- owlcms/src/main/java/app/owlcms/AppShell.java
+++ owlcms/src/main/java/app/owlcms/AppShell.java
@@ -1,21 +1,48 @@
 package app.owlcms;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.LoadingIndicatorConfiguration;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  */
 @SuppressWarnings("serial")
-@PWA(name = "Project Base for Vaadin", shortName = "Project Base")
+@PWA(name = "Olympic Weightlifting Competition Management System", shortName = "owlcms")
 @Push
-public class AppShell implements AppShellConfigurator {
+@Theme(themeClass=Lumo.class)
+public class AppShell implements AppShellConfigurator, VaadinServiceInitListener  {
     @Override
     public void configurePage(AppShellSettings settings) {
         settings.addLink("shortcut icon", "icons/owlcms.ico");
         settings.addFavIcon("icon", "icons/owlcms.png", "96x96");
+    }
+    
+    @Override
+    public void serviceInit(ServiceInitEvent serviceInitEvent) {
+        serviceInitEvent.getSource().addUIInitListener(uiInitEvent -> {
+            LoadingIndicatorConfiguration conf = uiInitEvent.getUI().getLoadingIndicatorConfiguration();
+
+            // disable default theme on loading indicator -> loading indicator isn't shown
+            conf.setApplyDefaultTheme(false);
+
+            /*
+             * Delay for showing the indicator and setting the 'first' class name.
+             */
+            conf.setFirstDelay(2000); // 300ms is the default
+
+            /* Delay for setting the 'second' class name */
+            conf.setSecondDelay(3500); // 1500ms is the default
+
+            /* Delay for setting the 'third' class name */
+            conf.setThirdDelay(5000); // 5000ms is the default
+        });
     }
 }
