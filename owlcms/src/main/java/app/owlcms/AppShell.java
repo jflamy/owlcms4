54008af9a6100e475a9f1e0598ebae312977e4c8
Jean-François Lamy <jf@jflamy.dev>
2022-12-27 11:59:23 -0500

    provide correct html lang attribute; closes #604

--- owlcms/src/main/java/app/owlcms/AppShell.java
+++ owlcms/src/main/java/app/owlcms/AppShell.java
@@ -6,10 +6,14 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.LoadingIndicatorConfiguration;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.AppShellSettings;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinRequest;
+import com.vaadin.flow.server.VaadinService;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.flow.server.VaadinServletResponse;
+import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.server.communication.IndexHtmlRequestListener;
+import com.vaadin.flow.server.communication.IndexHtmlResponse;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
@@ -20,10 +24,10 @@ import app.owlcms.init.OwlcmsSession;
  * and some desktop browsers.
  */
 @SuppressWarnings("serial")
-@PWA(name = "Olympic Weightlifting Competition Management System", shortName = "owlcms")
+//@PWA(name = "Olympic Weightlifting Competition Management System", shortName = "owlcms")
 @Push
 @Theme(themeClass=Lumo.class)
-public class AppShell implements AppShellConfigurator, VaadinServiceInitListener  {
+public class AppShell implements AppShellConfigurator, VaadinServiceInitListener, IndexHtmlRequestListener {
     
     /**
      * @see com.vaadin.flow.component.page.AppShellConfigurator#configurePage(com.vaadin.flow.server.AppShellSettings)
@@ -31,10 +35,15 @@ public class AppShell implements AppShellConfigurator, VaadinServiceInitListener
     @Override
     public void configurePage(AppShellSettings settings) {
         HttpServletResponse response = VaadinServletResponse.getCurrent().getHttpServletResponse();
-        response.addHeader("Content-Language",  OwlcmsSession.getLocale().toString().replace("_", "*"));
-        settings.addInlineWithContents("<meta http-equiv='content-language' content='"+OwlcmsSession.getLocale().toString().replace("_", "-")+"'>",  null);
+        response.addHeader("Content-Language",  getCurrentUserLanguage());
+        // not a recommended practice
+        //settings.addInlineWithContents("<meta http-equiv='content-language' content='"+getCurrentUserLanguage()+"'>",  null);
         settings.addLink("shortcut icon", "icons/owlcms.ico");
         settings.addFavIcon("icon", "icons/owlcms.png", "96x96");
+    }
+
+    private String getCurrentUserLanguage() {
+        return OwlcmsSession.getLocale().toString().replace("_", "-");
     }
     
     /**
@@ -45,8 +54,8 @@ public class AppShell implements AppShellConfigurator, VaadinServiceInitListener
         serviceInitEvent.getSource().addUIInitListener(uiInitEvent -> {
             LoadingIndicatorConfiguration conf = uiInitEvent.getUI().getLoadingIndicatorConfiguration();
 
-            // disable default theme on loading indicator -> loading indicator isn't shown
-//            conf.setApplyDefaultTheme(false);
+            //disable default theme on loading indicator -> loading indicator isn't shown
+            //conf.setApplyDefaultTheme(false);
 
             /*
              * Delay for showing the indicator and setting the 'first' class name.
@@ -59,5 +68,15 @@ public class AppShell implements AppShellConfigurator, VaadinServiceInitListener
             /* Delay for setting the 'third' class name */
             conf.setThirdDelay(5000); // 5000ms is the default
         });
+        serviceInitEvent.addIndexHtmlRequestListener(this);
+    }
+
+    /**
+     * @see com.vaadin.flow.server.communication.IndexHtmlRequestListener#modifyIndexHtmlResponse(com.vaadin.flow.server.communication.IndexHtmlResponse)
+     */
+    @Override
+    public void modifyIndexHtmlResponse(IndexHtmlResponse indexHtmlResponse) {
+        indexHtmlResponse.getDocument().getElementsByTag("html").attr("lang", getCurrentUserLanguage());
+        System.err.println("modified document "+ indexHtmlResponse.getDocument().getElementsByTag("html"));
     }
 }
