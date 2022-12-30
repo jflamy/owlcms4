4816e3ddc31cdc879a511af94fb62640d1eb973e
Jean-François Lamy <jf@jflamy.dev>
2022-12-30 16:33:05 -0500

    Edge instead of Chrome

--- launch/multichrome.bat
+++ launch/multichrome.bat
@@ -1,3 +1,10 @@
+rem start multiple independent browsers
+rem
+rem NOTE: Edge works much better than Chrome in terms of memory management
+rem BUT: when using Edge the screens don't start the same way and take a little longer, so a refresh is
+rem sometimes needed. 
+rem ===================================================================================================
+
 set SITE=http://localhost:8080
 set PLATFORM=A
 
@@ -12,18 +19,16 @@ rem we use the RANDOM variable to create new ones on every run
 rem to avoid accumulation, we remove files from previous runs
 rd /s /q "%tmp%\owlcms-tests"
 
-start "" "%CHROME%" --app="%SITE%/lifting/announcer?fop=%PLATFORM%" --window-size=920,565 --window-position=-5,0 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%
-start "" "%CHROME%" --app="%SITE%/lifting/jury?fop=%PLATFORM%" --window-size=920,650 --window-position=-5,560 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%
+start "" "%EDGE%" --app="%SITE%/lifting/announcer?fop=%PLATFORM%" --window-size=920,565 --window-position=-5,0 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%
+start "" "%EDGE%" --app="%SITE%/lifting/jury?fop=%PLATFORM%" --window-size=920,650 --window-position=-5,560 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%
 
-start "" "%CHROME%" --app="%SITE%/lifting/timekeeper?fop=%PLATFORM%" --window-size=640,398 --window-position=1200,358 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM% --force-device-scale-factor=0.64
+start "" "%EDGE%" --app="%SITE%/lifting/timekeeper?fop=%PLATFORM%" --window-size=640,398 --window-position=1200,358 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM% --force-device-scale-factor=0.64
 
-start "" "%CHROME%" --app="%SITE%/displays/attemptBoard?fop=%PLATFORM%" --window-size=500,300 --window-position=1375,270 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM% 
-start "" "%CHROME%" --app="%SITE%/displays/athleteFacingDecision?fop=%PLATFORM%" --window-size=400,300 --window-position=1865,270 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%
+start "" "%EDGE%" --app="%SITE%/displays/attemptBoard?fop=%PLATFORM%" --window-size=500,300 --window-position=1375,270 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM% 
+start "" "%EDGE%" --app="%SITE%/displays/athleteFacingDecision?fop=%PLATFORM%" --window-size=400,300 --window-position=1865,270 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%
 
-start "" "%CHROME%" --app="%SITE%/ref?fop=%PLATFORM%&num=1" --window-size=400,275 --window-position=905,1 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%  
-start "" "%CHROME%" --app="%SITE%/ref?fop=%PLATFORM%&num=2" --window-size=400,275 --window-position=1295,1 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%  
-start "" "%CHROME%" --app="%SITE%/ref?fop=%PLATFORM%&num=3" --window-size=400,275 --window-position=1685,1 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%
+start "" "%EDGE%" --app="%SITE%/ref?fop=%PLATFORM%&num=1" --window-size=400,275 --window-position=905,1 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%  
+start "" "%EDGE%" --app="%SITE%/ref?fop=%PLATFORM%&num=2" --window-size=400,275 --window-position=1295,1 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%  
+start "" "%EDGE%" --app="%SITE%/ref?fop=%PLATFORM%&num=3" --window-size=400,275 --window-position=1685,1 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM%
 
-start "" "%CHROME%" --app="%SITE%/displays/resultsLeadersRanks?fop=%PLATFORM%&dark=false&em=0.90" --window-size=1280,720 --window-position=1025,650 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM% --force-device-scale-factor=0.75
-
-
+start "" "%EDGE%" --app="%SITE%/displays/resultsLeadersRanks?fop=%PLATFORM%&dark=false&em=0.90" --window-size=1280,720 --window-position=1025,645 %commonoptions% --user-data-dir=%tmp%\owlcms-tests\%RANDOM% --force-device-scale-factor=0.75
