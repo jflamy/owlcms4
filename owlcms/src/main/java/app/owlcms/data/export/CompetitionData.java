053c4a1750a783f0ad66455ad597a2f3ee168564
Jean-François Lamy <jf@jflamy.dev>
2023-05-14 14:18:41 -0400

    accept new fields - attempt forward compatibility

--- owlcms/src/main/java/app/owlcms/data/export/CompetitionData.java
+++ owlcms/src/main/java/app/owlcms/data/export/CompetitionData.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -40,6 +41,7 @@ import app.owlcms.init.OwlcmsFactory;
 import app.owlcms.utils.ResourceWalker;
 import ch.qos.logback.classic.Logger;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CompetitionData {
 
 	final static Logger logger = (Logger) LoggerFactory.getLogger(CompetitionData.class);
