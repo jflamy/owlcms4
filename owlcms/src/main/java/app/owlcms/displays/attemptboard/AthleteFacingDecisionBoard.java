0bc3fedf636e2f187b152454e4fbd512a67e1504
Jean-François Lamy <jf@jflamy.dev>
2023-06-27 13:40:56 -0400

    fixed initial display on startup

--- owlcms/src/main/java/app/owlcms/displays/attemptboard/AthleteFacingDecisionBoard.java
+++ owlcms/src/main/java/app/owlcms/displays/attemptboard/AthleteFacingDecisionBoard.java
@@ -11,6 +11,9 @@ import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.router.Route;
 
+import app.owlcms.data.competition.Competition;
+import app.owlcms.fieldofplay.FOPState;
+import app.owlcms.fieldofplay.FieldOfPlay;
 import app.owlcms.init.OwlcmsSession;
 
 @SuppressWarnings("serial")
@@ -59,5 +62,15 @@ public class AthleteFacingDecisionBoard extends AttemptBoard {
 	protected void onAttach(AttachEvent attachEvent) {
 		super.onAttach(attachEvent);
 		decisions.setPublicFacing(false);
+	}
+	
+	protected void doEmpty() {
+		FieldOfPlay fop2 = OwlcmsSession.getFop();
+		boolean inactive = fop2 == null || fop2.getState() == FOPState.INACTIVE;
+		this.getElement().callJsFunction("clear");
+		this.getElement().setProperty("inactiveBlockStyle", (inactive ? "display:grid" : "display:none"));
+		this.getElement().setProperty("activeGridStyle", (inactive ? "display:none" : "display:grid"));
+		this.getElement().setProperty("inactiveClass", (inactive ? "bigTitle" : ""));
+		this.getElement().setProperty("competitionName", Competition.getCurrent().getCompetitionName());
 	}
 }
--- owlcms/src/main/java/app/owlcms/displays/attemptboard/PublicFacingDecisionBoard.java
+++ owlcms/src/main/java/app/owlcms/displays/attemptboard/PublicFacingDecisionBoard.java
@@ -11,7 +11,10 @@ import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.router.Route;
 
+import app.owlcms.data.competition.Competition;
 import app.owlcms.data.config.Config;
+import app.owlcms.fieldofplay.FOPState;
+import app.owlcms.fieldofplay.FieldOfPlay;
 import app.owlcms.init.OwlcmsSession;
 
 @SuppressWarnings("serial")
@@ -60,8 +63,16 @@ public class PublicFacingDecisionBoard extends AttemptBoard {
 		setPublicFacing(true);
 		setShowBarbell(false);
 		setSilenced(isSilencedByDefault());
-		
-
+	}
+	
+	protected void doEmpty() {
+		FieldOfPlay fop2 = OwlcmsSession.getFop();
+		boolean inactive = fop2 == null || fop2.getState() == FOPState.INACTIVE;
+		this.getElement().callJsFunction("clear");
+		this.getElement().setProperty("inactiveBlockStyle", (inactive ? "display:grid" : "display:none"));
+		this.getElement().setProperty("activeGridStyle", (inactive ? "display:none" : "display:grid"));
+		this.getElement().setProperty("inactiveClass", (inactive ? "bigTitle" : ""));
+		this.getElement().setProperty("competitionName", Competition.getCurrent().getCompetitionName());
 	}
 
 }
