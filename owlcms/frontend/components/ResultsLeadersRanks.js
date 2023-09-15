7cc5d3a59bdaec3ba5beb3129818d54ce0b0a737
Jean-François Lamy <jf@jflamy.dev>
2023-09-14 20:29:01 -0400

    removed openDialog

--- owlcms/frontend/components/ResultsLeadersRanks.js
+++ owlcms/frontend/components/ResultsLeadersRanks.js
@@ -17,7 +17,7 @@ class ResultsFull extends LitElement {
       <link rel="stylesheet" type="text/css" .href="${"local/" + (this.stylesDir ?? "") + "/" + (this.video ?? "") + "results" + (this.autoversion ?? "") + ".css"}" />
       <link rel="stylesheet" type="text/css" .href="${"local/" + (this.stylesDir ?? "") + "/" + (this.video ?? "") + "resultsRanksCustomization" + (this.autoversion ?? "") + ".css"}" />
 
-      <div class="${this.wrapperClasses()}"  style="${this.sizeOverride}" @click="${this._handleClick}">
+      <div class="${this.wrapperClasses()}"  style="${this.sizeOverride}">
         <div class="blockPositioningWrapper">
           <div class="waiting" style="${this.waitingStyles()}">
             <div>
@@ -380,7 +380,6 @@ class ResultsFull extends LitElement {
     console.debug("ready");
     super.firstUpdated(_changedProperties);
     document.body.setAttribute("theme", "dark");
-    this.$server.openDialog();
   }
 
   start() {
@@ -489,10 +488,6 @@ class ResultsFull extends LitElement {
 
   isCountdown() {
     return  this.mode === "INTRO_COUNTDOWN" || this.mode === "LIFT_COUNTDOWN"
-  }
-
-  _handleClick() {
-    this.$server.openDialog();
   }
 
   constructor() {
