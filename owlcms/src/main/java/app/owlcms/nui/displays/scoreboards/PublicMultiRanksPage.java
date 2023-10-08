c1950eaa761c0eb06db583e7a7c474dbb968caa8
Jean-François Lamy <jf@jflamy.dev>
2023-10-08 10:10:28 -0400

    don't switch public board unless medals; closes #757

--- owlcms/src/main/java/app/owlcms/nui/displays/scoreboards/PublicMultiRanksPage.java
+++ owlcms/src/main/java/app/owlcms/nui/displays/scoreboards/PublicMultiRanksPage.java
@@ -15,6 +15,7 @@ import app.owlcms.data.config.Config;
 import app.owlcms.displays.scoreboard.ResultsMedals;
 import app.owlcms.displays.scoreboard.ResultsMultiRanks;
 import app.owlcms.init.OwlcmsSession;
+import app.owlcms.uievents.CeremonyType;
 import app.owlcms.uievents.UIEvent;
 import ch.qos.logback.classic.Logger;
 
@@ -25,7 +26,7 @@ public class PublicMultiRanksPage extends AbstractResultsDisplayPage {
 
 	Logger logger;
 	Logger uiEventLogger;
-	
+
 	private ResultsMultiRanks resultsBoard;
 	private ResultsMedals medalsBoard;
 	protected UI ui;
@@ -35,10 +36,39 @@ public class PublicMultiRanksPage extends AbstractResultsDisplayPage {
 		return getTranslation("ScoreboardMultiRanksTitle") + OwlcmsSession.getFopNameIfMultiple();
 	}
 
+	public final ResultsMultiRanks getResultsBoard() {
+		return this.resultsBoard;
+	}
+
+	@Subscribe
+	public void slaveCeremonyDone(UIEvent.CeremonyDone e) {
+		if (e.getCeremonyType() != CeremonyType.MEDALS) {
+			return;
+		}
+		this.ui.access(() -> {
+			getMedalsBoard().setVisible(false);
+			getResultsBoard().setVisible(true);
+		});
+	}
+
+	@Subscribe
+	public void slaveCeremonyStarted(UIEvent.CeremonyStarted e) {
+		if (e.getCeremonyType() != CeremonyType.MEDALS) {
+			return;
+		}
+		this.ui.access(() -> {
+			getMedalsBoard().setDarkMode(getResultsBoard().isDarkMode());
+			getMedalsBoard().setTeamWidth(getResultsBoard().getTeamWidth());
+			getMedalsBoard().setEmFontSize(getResultsBoard().getEmFontSize());
+			getMedalsBoard().setVisible(true);
+			getResultsBoard().setVisible(false);
+		});
+	}
+
 	@Override
 	protected void init() {
-		logger = (Logger) LoggerFactory.getLogger(PublicScoreboardPage.class);
-		uiEventLogger = (Logger) LoggerFactory.getLogger("UI" + this.logger.getName());
+		this.logger = (Logger) LoggerFactory.getLogger(PublicScoreboardPage.class);
+		this.uiEventLogger = (Logger) LoggerFactory.getLogger("UI" + this.logger.getName());
 
 		// each subclass must override this routine.
 		// otherwise we end up with multiple instances of the Results board.
@@ -53,50 +83,27 @@ public class PublicMultiRanksPage extends AbstractResultsDisplayPage {
 		this.ui = UI.getCurrent();
 
 		setDefaultParameters(QueryParameters.simple(Map.of(
-		        SoundParameters.SILENT, "true",
-		        SoundParameters.DOWNSILENT, "true",
-		        DisplayParameters.DARK, "true",
-		        DisplayParameters.LEADERS, "true",
-		        DisplayParameters.RECORDS, "true",
-		        DisplayParameters.SINGLEREF, "false",
-		        DisplayParameters.PUBLIC, "false",
-		        DisplayParameters.ABBREVIATED,
-		        Boolean.toString(Config.getCurrent().featureSwitch("shortScoreboardNames")))));
+				SoundParameters.SILENT, "true",
+				SoundParameters.DOWNSILENT, "true",
+				DisplayParameters.DARK, "true",
+				DisplayParameters.LEADERS, "true",
+				DisplayParameters.RECORDS, "true",
+				SoundParameters.SINGLEREF, "false",
+				DisplayParameters.PUBLIC, "false",
+				DisplayParameters.ABBREVIATED,
+				Boolean.toString(Config.getCurrent().featureSwitch("shortScoreboardNames")))));
 	}
 
-	@Subscribe
-	public void slaveCeremonyDone(UIEvent.CeremonyDone e) {
-		this.ui.access(() -> {
-			getMedalsBoard().setVisible(false);
-			getResultsBoard().setVisible(true);
-		});
+	private final ResultsMedals getMedalsBoard() {
+		return this.medalsBoard;
 	}
-	
+
 	private void setMedalsBoard(ResultsMedals medalsBoard) {
 		this.medalsBoard = medalsBoard;
 	}
 
 	private void setResultsBoard(ResultsMultiRanks board) {
 		this.resultsBoard = board;
-	}
-
-	public final ResultsMultiRanks getResultsBoard() {
-		return resultsBoard;
-	}
-
-	private final ResultsMedals getMedalsBoard() {
-		return medalsBoard;
-	}
-
-	@Subscribe
-	public void slaveCeremonyStarted(UIEvent.CeremonyStarted e) {
-		this.ui.access(() -> {
-			getMedalsBoard().setDarkMode(getResultsBoard().isDarkMode());
-			getMedalsBoard().setTeamWidth(getResultsBoard().getTeamWidth());
-			getMedalsBoard().setEmFontSize(getResultsBoard().getEmFontSize());
-			getMedalsBoard().setVisible(true);
-			getResultsBoard().setVisible(false);
-		});
 	}
 
 }
