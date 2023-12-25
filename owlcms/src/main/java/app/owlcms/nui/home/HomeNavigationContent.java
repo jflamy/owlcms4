c3ffa47411ce673148f013c8dec5b5d39ebb869f
Jean-François Lamy <jf@jflamy.dev>
2023-12-25 10:54:27 -0500

    fix timezone logging

--- owlcms/src/main/java/app/owlcms/nui/home/HomeNavigationContent.java
+++ owlcms/src/main/java/app/owlcms/nui/home/HomeNavigationContent.java
@@ -20,6 +20,7 @@ import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -51,7 +52,6 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinRequest;
 
 import app.owlcms.apputils.DebugUtils;
-import app.owlcms.data.config.Config;
 import app.owlcms.i18n.Translator;
 import app.owlcms.init.OwlcmsFactory;
 import app.owlcms.nui.displays.DisplayNavigationContent;
@@ -91,9 +91,9 @@ public class HomeNavigationContent extends BaseNavigationContent implements Navi
 	public static FlexibleGridLayout navigationGrid(Component... items) {
 		FlexibleGridLayout layout = new FlexibleGridLayout();
 		layout.withColumns(Repeat.RepeatMode.AUTO_FILL, new MinMax(new Length("300px"), new Flex(1)))
-		.withAutoRows(new Length("1fr")).withItems(items)
-		.withOverflow(Overflow.AUTO).withAutoFlow(AutoFlow.ROW).withMargin(false).withPadding(true)
-		.withSpacing(false);
+		        .withAutoRows(new Length("1fr")).withItems(items)
+		        .withOverflow(Overflow.AUTO).withAutoFlow(AutoFlow.ROW).withMargin(false).withPadding(true)
+		        .withSpacing(false);
 		layout.getContent().setGap(new Length("0.5em"), new Length("1.0em"));
 		layout.setSizeUndefined();
 		layout.setWidth("80%");
@@ -109,11 +109,8 @@ public class HomeNavigationContent extends BaseNavigationContent implements Navi
 	String VIDEO_STREAMING = Translator.translate("VideoStreaming");
 	String START_DISPLAYS = Translator.translate("StartDisplays");
 	Map<String, List<String>> urlParameterMap = new HashMap<>();
-
 	String referenceVersionString;
-
 	String currentVersionString = "";
-
 	int comparison = 999;
 
 	/**
@@ -124,15 +121,15 @@ public class HomeNavigationContent extends BaseNavigationContent implements Navi
 		intro.setSpacing(false);
 
 		Button prepare = new Button(this.PREPARE_COMPETITION,
-				buttonClickEvent -> UI.getCurrent().navigate(PreparationNavigationContent.class));
+		        buttonClickEvent -> UI.getCurrent().navigate(PreparationNavigationContent.class));
 		Button displays = new Button(this.START_DISPLAYS,
-				buttonClickEvent -> UI.getCurrent().navigate(DisplayNavigationContent.class));
+		        buttonClickEvent -> UI.getCurrent().navigate(DisplayNavigationContent.class));
 		Button video = new Button(this.VIDEO_STREAMING,
-				buttonClickEvent -> UI.getCurrent().navigate(VideoNavigationContent.class));
+		        buttonClickEvent -> UI.getCurrent().navigate(VideoNavigationContent.class));
 		Button lifting = new Button(this.RUN_LIFTING_GROUP,
-				buttonClickEvent -> UI.getCurrent().navigate(LiftingNavigationContent.class));
+		        buttonClickEvent -> UI.getCurrent().navigate(LiftingNavigationContent.class));
 		Button documents = new Button(this.RESULT_DOCUMENTS,
-				buttonClickEvent -> UI.getCurrent().navigate(ResultsNavigationContent.class));
+		        buttonClickEvent -> UI.getCurrent().navigate(ResultsNavigationContent.class));
 		FlexibleGridLayout grid = HomeNavigationContent.navigationGrid(prepare, lifting, displays, video, documents);
 
 		fillH(intro, this);
@@ -145,6 +142,7 @@ public class HomeNavigationContent extends BaseNavigationContent implements Navi
 	public String getMenuTitle() {
 		return getTranslation("OWLCMS_Top");
 	}
+
 	/**
 	 * @see app.owlcms.nui.shared.BaseNavigationContent#getPageTitle()
 	 */
@@ -152,6 +150,7 @@ public class HomeNavigationContent extends BaseNavigationContent implements Navi
 	public String getPageTitle() {
 		return getTranslation("OWLCMS_Top");
 	}
+
 	/**
 	 * @see app.owlcms.apputils.queryparameters.FOPParameters#isIgnoreFopFromURL()
 	 */
@@ -203,19 +202,19 @@ public class HomeNavigationContent extends BaseNavigationContent implements Navi
 		hr.getStyle().set("margin-bottom", "2ex");
 		intro.add(hr);
 		addP(intro,
-				getTranslation("LeftMenuNavigate")
-				+ getTranslation("PrepareCompatition_description", this.PREPARE_COMPETITION)
-				+ getTranslation("RunLiftingGroup_description", this.RUN_LIFTING_GROUP)
-				+ getTranslation("StartDisplays_description", this.START_DISPLAYS)
-				+ getTranslation("VideoStreaming_description", this.VIDEO_STREAMING)
-				+ getTranslation("CompetitionDocuments_description", this.RESULT_DOCUMENTS)
-				+ getTranslation("SeparateLaptops"));
+		        getTranslation("LeftMenuNavigate")
+		                + getTranslation("PrepareCompatition_description", this.PREPARE_COMPETITION)
+		                + getTranslation("RunLiftingGroup_description", this.RUN_LIFTING_GROUP)
+		                + getTranslation("StartDisplays_description", this.START_DISPLAYS)
+		                + getTranslation("VideoStreaming_description", this.VIDEO_STREAMING)
+		                + getTranslation("CompetitionDocuments_description", this.RESULT_DOCUMENTS)
+		                + getTranslation("SeparateLaptops"));
 		intro.getStyle().set("margin-bottom", "-1em");
 		return intro;
 	}
 
 	private Div checkVersion() {
-		//if (Config.getCurrent().featureSwitch("checkForUpdate"))
+		// if (Config.getCurrent().featureSwitch("checkForUpdate"))
 		{
 			this.currentVersionString = OwlcmsFactory.getVersion();
 			String suffix = this.currentVersionString.contains("-") ? "-prerelease" : "";
@@ -226,36 +225,37 @@ public class HomeNavigationContent extends BaseNavigationContent implements Navi
 			CompletableFuture<HttpResponse<String>> future = client1.sendAsync(request1, BodyHandlers.ofString());
 			try {
 				future
-				.orTimeout(200, TimeUnit.MILLISECONDS)
-				.whenComplete((response, exception) -> {
-					if (exception != null) {
-						return;
-					}
-					ComparableVersion currentVersion = new ComparableVersion(this.currentVersionString);
-					this.referenceVersionString = response.body();
-					ComparableVersion referenceVersion = new ComparableVersion(this.referenceVersionString);
-					this.comparison = currentVersion.compareTo(referenceVersion);
-				})
-				.join();
+				        .orTimeout(200, TimeUnit.MILLISECONDS)
+				        .whenComplete((response, exception) -> {
+					        if (exception != null) {
+						        return;
+					        }
+					        ComparableVersion currentVersion = new ComparableVersion(this.currentVersionString);
+					        this.referenceVersionString = response.body();
+					        ComparableVersion referenceVersion = new ComparableVersion(this.referenceVersionString);
+					        this.comparison = currentVersion.compareTo(referenceVersion);
+				        })
+				        .join();
 			} catch (Throwable e) {
 				logger.error("version fetch timed out");
 			}
 		}
 		Div div = new Div();
 		if (
-				//(Config.getCurrent().featureSwitch("checkForUpdate")) &&
-				this.comparison < 999) {
+		// (Config.getCurrent().featureSwitch("checkForUpdate")) &&
+		this.comparison < 999) {
 			String runningMsg = Translator.translate("CheckVersion.running", this.currentVersionString);
 			String referenceVersionMsg = Translator.translate(
-					"CheckVersion.reference" + (this.referenceVersionString.contains("-") ? "Prerelease" : "Stable"),
-					this.referenceVersionString);
+			        "CheckVersion.reference" + (this.referenceVersionString.contains("-") ? "Prerelease" : "Stable"),
+			        this.referenceVersionString);
 			String okVersionMsg = Translator.translate("CheckVersion.ok");
 			String behindVersionMsg = Translator.translate("CheckVersion.behind");
 			String aheadVersionMsg = Translator.translate("CheckVersion.ahead");
 
 			String formatted = MessageFormat.format(
-					"{1} {0, choice, 0#{2} {3}|1#{4}|2#{2} {5}}",
-					this.comparison + 1, runningMsg, referenceVersionMsg, behindVersionMsg, okVersionMsg, aheadVersionMsg);
+			        "{1} {0, choice, 0#{2} {3}|1#{4}|2#{2} {5}}",
+			        this.comparison + 1, runningMsg, referenceVersionMsg, behindVersionMsg, okVersionMsg,
+			        aheadVersionMsg);
 			div.setText(formatted);
 			if (this.comparison < 0) {
 				div.getStyle().set("font-weight", "bold");
@@ -293,20 +293,26 @@ public class HomeNavigationContent extends BaseNavigationContent implements Navi
 				// can't happen, will be a numerical address
 			}
 		}
+		
+		// the origin will be localhost (ipv4 or ipv6) or an ip local address when running locally
+		// the remote logger will use the x-forwarded-for header to obtain the public ip address
 		if (a != null && (a.isLoopbackAddress() || a.isSiteLocalAddress() || a.isLinkLocalAddress())) {
 			local = true;
 		}
 
-		// log versions in use for statistical purposes
-		// origin will be localhost (ipv4 or ipv6) or an ip local address except when running on the cloud.
-		String tzId = Config.getCurrent().getTimeZone().getID().replaceAll("/", "_");
+		// When running in the cloud,the remote logger gets the cloud server as the originating address, which is
+		// useless.
+		// But owlcms receives the browser address in x-forwarded-for so owlcms injects the browser in the logging data.
+		
+		// The default time zone has already been overridden if specified in the database or environment.
+		String tzId = TimeZone.getDefault().getID().replaceAll("/", "_");;
 		String usageStr = "https://usage.lerta.ca?"
-				+ "&version=" + this.currentVersionString
-				+ "&localtime=" + LocalTime.now().toString()
-				+ "&localdate=" + LocalDate.now().toString()
-				+ "&timezone=" + tzId 
-				+ (local ? "" : "&origin="+ipAddress)
-				;
+		        + "&version=" + this.currentVersionString
+		        + "&localdate=" + LocalDate.now().toString()
+		        + "&localtime=" + LocalTime.now().toString()
+		        + "&timezone=" + tzId
+		        + (local ? "" : "&origin=" + ipAddress);
+		logger.info("logging {}", usageStr);
 		HttpRequest usageRequest = HttpRequest.newBuilder(URI.create(usageStr)).timeout(Duration.ofMillis(200)).build();
 		// fire and forget
 		client.sendAsync(usageRequest, BodyHandlers.ofString());
