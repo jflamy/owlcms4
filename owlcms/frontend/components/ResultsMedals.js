be0a70375557efe1606268c4e7336b7b9eaf18cc
Jean-François Lamy <jf@jflamy.dev>
2024-02-19 11:56:36 -0500

    branding on scoreboards

--- owlcms/frontend/components/ResultsMedals.js
+++ owlcms/frontend/components/ResultsMedals.js
@@ -159,6 +159,9 @@ class ResultsMedals extends LitElement {
                 </table>
               `
             : html``}
+            <div style="line-height: 1.25em">&nbsp;
+              <div style="position: absolute; bottom: 0.5em; right: 1em; display: flex; align-items: center; font-weight: thin; font-size: 0.9em;"><img src="local/logos/owlcms-logo.svg" style="height:1.25em; margin-bottom:-0.2em">&nbsp;owlcms</div>
+            </div>
         </div>
       </div>`;
   }
