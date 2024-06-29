637c779a7d8bcf4b01956846ba874dcf17a3a1a3
Jean-François Lamy <jf@jflamy.dev>
2024-06-29 13:59:32 -0400

    enumerated events

--- publicresults/src/main/java/app/owlcms/prutils/SafeEventBusRegistrationPR.java
+++ publicresults/src/main/java/app/owlcms/prutils/SafeEventBusRegistrationPR.java
@@ -33,11 +33,12 @@ public interface SafeEventBusRegistrationPR {
                     bus.identifier(), LoggerUtils.whereFrom());
             bus.register(c);
         }
-        
+
         UnloadObserverPR eventObserver = UnloadObserverPR.get(true);
         eventObserver.addEventListener((e) -> {
             String change = e.getChange();
-            if (change.equals("beforeunload") || change.equals("blur")) {
+            if (change.equals("beforeunload")) {
+                // actual killing is handled by the detach listener
                 try {
                     unregister(c, bus);
                     logger.warn("{}: unregister {} from {}", change, c.getClass().getSimpleName(), bus.identifier());
@@ -45,28 +46,61 @@ public interface SafeEventBusRegistrationPR {
                     LoggerUtils.logError(logger, ex, true);
                 }
                 UnloadObserverPR.remove();
+            } else if (change.equals("visibilityHidden")) {
+                // switching tabs or minimizing window. no visible scoreboard
+                setInactivityTimer();
+                try {
+                    logger.warn("{}: setInactivityTimer {} from {}", change, c.getClass().getSimpleName(), bus.identifier());
+                } catch (Exception ex) {
+                    LoggerUtils.logError(logger, ex, true);
+                }
+            } else if (change.equals("blur")) {
+                // blurring can occur when switching the active window and still watching.
+                // we probably don't want to do anything
+                try {
+                    logger.warn("{}: do nothing from {}", change, c.getClass().getSimpleName(), bus.identifier());
+                } catch (Exception ex) {
+                    LoggerUtils.logError(logger, ex, true);
+                }
+            } else if (change.equals("visibilityShown")) {
+                cancelInactivityTimer();
+                try {
+                    logger.warn("{}: cancelInactivityTimer {} from {}", change, c.getClass().getSimpleName(), bus.identifier());
+                } catch (Exception ex) {
+                    LoggerUtils.logError(logger, ex, true);
+                }
+            } else if (change.equals("focus")) {
+                cancelInactivityTimer();
+                try {
+                    logger.warn("{}: cancelInactivityTimer {} from {}", change, c.getClass().getSimpleName(), bus.identifier());
+                } catch (Exception ex) {
+                    LoggerUtils.logError(logger, ex, true);
+                }
             } else {
                 logger.warn("{} {} {}", change, c.getClass().getSimpleName(), System.identityHashCode(c));
             }
         });
-        
+
         ui.access(() -> {
             ui.add(eventObserver);
 
             ui.addBeforeLeaveListener((e) -> {
+                // navigating via a link, don't kill session, clean-up this page.
+                // should only happen on the main page.
                 try {
                     unregister(c, bus);
                     ui.access(() -> {
                         UnloadObserverPR.remove();
                         ui.removeAll();
                     });
-                    logger.warn("leaving: unregistered {} {}", c.getClass().getSimpleName(),
+                    logger.warn("leaving: unregistering {} {}", c.getClass().getSimpleName(),
                             System.identityHashCode(c));
                 } catch (Exception ex) {
                     LoggerUtils.logError(logger, ex, true);
                 }
             });
             ui.addDetachListener((e) -> {
+                // actually leaving the page. kill publicresults session.
                 try {
                     logger.warn("invalidating: invalidating session for {} {}", c.getClass().getSimpleName(),
                             System.identityHashCode(c));
@@ -85,6 +119,10 @@ public interface SafeEventBusRegistrationPR {
         });
         return bus;
     }
+
+    public void cancelInactivityTimer();
+
+    public void setInactivityTimer();
 
     public default void unregister(Component c, EventBus bus) {
         try {
