8b8a2bac6c9e8c04acc792daa0d3c66c8ad690ab
Jean-François Lamy <jf@jflamy.dev>
2024-09-28 09:11:38 -0400

    cleanup

--- deployPR.sh
+++ deployPR.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -
-fly deploy . --local-only --dockerfile DockerfilePR --env OWLCMS_RESTARTSIZE=512 --app owlcms-next-results --config owlcms-next-results.toml --ha=false 
-#--image-label $1
+fly deploy . --local-only --dockerfile DockerfilePR --env OWLCMS_RESTARTSIZE=768 --app nextpr --config owlcms-next-results.toml --ha=false
