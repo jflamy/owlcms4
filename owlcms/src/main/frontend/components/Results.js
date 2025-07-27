1812590dd7f109d5350ffe8f4e79403bb290d087
Jean-François Lamy <jf@jflamy.dev>
2025-07-26 21:29:52 -0400

    move leader filler to correct location

--- owlcms/src/main/frontend/components/Results.js
+++ owlcms/src/main/frontend/components/Results.js
@@ -164,11 +164,11 @@ class Results extends LitElement {
                   `)}
               `
               : html``}
-            <tr>
-              <td class="filler" .style="grid-column: 1 / -1; ${this.fillerStyles()}"> &nbsp; </td>
-            </tr>
             ${this.leaders
               ? html`
+                <tr>
+                  <td class="filler" .style="grid-column: 1 / -1; ${this.fillerStyles()}"> &nbsp; </td>
+                </tr>
                 <tbody class="leaders" style="${this.leadersStyles()}">
                   <tr class="head">
                     <td class="leaderTitle" .innerHTML="${(this.t?.Leaders ?? "") + " " + (this.categoryName ?? "")}"></td>
