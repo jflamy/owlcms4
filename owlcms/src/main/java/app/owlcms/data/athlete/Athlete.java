730a23569a1b1a8b9597e49854d076b73b7b543c
Jean-François Lamy <jf@jflamy.dev>
2025-10-19 18:09:10 -0400

    jsonignore

--- owlcms/src/main/java/app/owlcms/data/athlete/Athlete.java
+++ owlcms/src/main/java/app/owlcms/data/athlete/Athlete.java
@@ -2292,6 +2292,8 @@ public class Athlete {
 		return this.participations;
 	}
 
+	@Transient
+	@JsonIgnore
 	public List<Participation> getCleanParticipations() {
 		return this.participations.stream()
 			.filter(p -> p.getCategory() != null && p.getCategory().getAgeGroup() != null)
