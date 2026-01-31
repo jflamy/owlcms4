2e165c9c14c56c850af8d484d77b2cb149a00df3
Jean-François Lamy <jf@jflamy.dev>
2026-01-31 17:44:33 -0500

    64.0.4-rc01

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-REVISION="${REVISION:-${1:-64.0.3}}"
+REVISION="${REVISION:-${1:-64.0.4-rc01}}"
 set -euo pipefail
 
 # Triggers the GitHub Actions workflow `.github/workflows/release.yaml`
