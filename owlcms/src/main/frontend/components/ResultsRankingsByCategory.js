aae25ec62d830ae6ab55c0984a10e5e55133fcb7
Jean-François Lamy <jf@jflamy.dev>
2026-02-19 19:36:12 -0500

    add branding class

--- owlcms/src/main/frontend/components/ResultsRankingsByCategory.js
+++ owlcms/src/main/frontend/components/ResultsRankingsByCategory.js
@@ -167,7 +167,7 @@ class ResultsRankingsByCategory extends LitElement {
               `
             : html``}
             <div style="${this.bottomSpacerStyles()}">&nbsp;
-              <div style="position: absolute; bottom: 0.5em; right: 1em; display: flex; align-items: center; font-weight: 100; font-size: 1.6vh;"><img src="local/logos/owlcms-logo.svg" style="height:1.25em; margin-bottom:-0.2em">&nbsp;owlcms</div>
+              <div class="branding" style="position: absolute; bottom: 0.5em; right: 1em; display: flex; align-items: center; font-weight: 100; font-size: 1.6vh;"><img src="local/logos/owlcms-logo.svg" style="height:1.25em; margin-bottom:-0.2em">&nbsp;owlcms</div>
             </div>
         </div>
       </div>`;
