bde3f2eb9eb1e13ca8c1fd09336689a919836fe6
Jean-François Lamy <jf@jflamy.dev>
2026-05-09 17:38:49 -0400

    sticky duration to match LED wall

--- owlcms/src/main/java/app/owlcms/nui/displays/scoreboards/NCurrentAthletePage.java
+++ owlcms/src/main/java/app/owlcms/nui/displays/scoreboards/NCurrentAthletePage.java
@@ -19,6 +19,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.AccessDeniedException;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.Location;
 import com.vaadin.flow.router.QueryParameters;
 import com.vaadin.flow.router.Route;
 
@@ -35,9 +36,22 @@ import ch.qos.logback.classic.Logger;
 @Route("displays/ncurrentathlete")
 public class NCurrentAthletePage extends AbstractResultsDisplayPage implements BeforeEnterObserver {
 
+	private static final String DECISION_STICK_MILLIS = "decision";
+    private static final String DECISION_STICK_TRACE_MARKER = "decision-stick-trace-2026-05-09";
+    private static final int DEFAULT_DECISION_STICK_MILLIS = 5000;
+
     Logger logger = (Logger) LoggerFactory.getLogger(NCurrentAthletePage.class);
     Logger uiEventLogger = (Logger) LoggerFactory.getLogger("UI" + this.logger.getName());
     Map<String, List<String>> urlParameterMap = new HashMap<>();
+    private int decisionStickMillis = DEFAULT_DECISION_STICK_MILLIS;
+
+    private String traceLabel() {
+        return "NCurrentAthletePage[" + System.identityHashCode(this) + "|" + DECISION_STICK_TRACE_MARKER + "]";
+    }
+
+    private static String fopName(FieldOfPlay fieldOfPlay) {
+        return fieldOfPlay != null ? fieldOfPlay.getName() : "null";
+    }
 
     @Override
     public void addDialogContent(Component target, VerticalLayout vl) {
@@ -61,6 +75,7 @@ public class NCurrentAthletePage extends AbstractResultsDisplayPage implements B
     protected void init() {
         this.logger = (Logger) LoggerFactory.getLogger(NCurrentAthletePage.class);
         this.uiEventLogger = (Logger) LoggerFactory.getLogger("UI" + this.logger.getName());
+        this.logger.warn("{} init", traceLabel());
         var board = new NCurrentAthlete(this);
         this.setBoard(board);
 
@@ -95,8 +110,23 @@ public class NCurrentAthletePage extends AbstractResultsDisplayPage implements B
         board.setFop(this.getFop());
         board.setLeadersDisplay(true);
         board.setRecordsDisplay(true);
+        this.logger.warn("{} onAttach ui={} fop={} decisionStickMillis={} boardInstance={}", traceLabel(),
+            attachEvent.getUI().getUIId(), fopName(this.getFop()), this.decisionStickMillis,
+            System.identityHashCode(board));
+        ((NCurrentAthlete) board).setDecisionStickMillis(this.decisionStickMillis);
 
         this.addComponentAsFirst((Component) board);
+    }
+
+    @Override
+    public Map<String, List<String>> readParams(Location location, Map<String, List<String>> parametersMap) {
+        Map<String, List<String>> params = super.readParams(location, parametersMap);
+        readDecisionStickMillis(params);
+        this.logger.warn("{} readParams path={} paramsDecision={} effectiveDecisionStickMillis={} boardInstance={}", traceLabel(),
+                location.getPath(), params.get(DECISION_STICK_MILLIS), this.decisionStickMillis,
+                System.identityHashCode(this.getBoard()));
+        ((NCurrentAthlete) this.getBoard()).setDecisionStickMillis(this.decisionStickMillis);
+        return params;
     }
 
 	@Override
@@ -104,7 +134,28 @@ public class NCurrentAthletePage extends AbstractResultsDisplayPage implements B
 		if (!Config.getCurrent().featureSwitch("iwfLook")) {
 			throw new AccessDeniedException();
 		}
+        readDecisionStickMillis(event.getLocation().getQueryParameters().getParameters());
+		this.logger.warn("{} beforeEnter path={} paramsDecision={} effectiveDecisionStickMillis={}", traceLabel(),
+		        event.getLocation().getPath(), event.getLocation().getQueryParameters().getParameters().get(DECISION_STICK_MILLIS),
+		        this.decisionStickMillis);
 	}
+
+    private void readDecisionStickMillis(Map<String, List<String>> params) {
+        this.decisionStickMillis = DEFAULT_DECISION_STICK_MILLIS;
+        List<String> decisionValues = params.get(DECISION_STICK_MILLIS);
+        if (decisionValues == null || decisionValues.isEmpty()) {
+            this.logger.warn("{} readDecisionStickMillis no parameter; defaulting to {}", traceLabel(),
+                    this.decisionStickMillis);
+            return;
+        }
+        try {
+            this.decisionStickMillis = Math.max(0, Integer.parseInt(decisionValues.get(0)));
+        } catch (NumberFormatException ignored) {
+            this.decisionStickMillis = DEFAULT_DECISION_STICK_MILLIS;
+        }
+        this.logger.warn("{} readDecisionStickMillis raw={} effective={}", traceLabel(), decisionValues,
+                this.decisionStickMillis);
+    }
     
     
     
