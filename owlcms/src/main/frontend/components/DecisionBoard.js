6ce1c714edee38d7f098a9a4fdaaa3ba2e16c698
Jean-François Lamy <jf@jflamy.dev>
2026-09-05 16:49:59 -0400

    Fix decision board countdown display

--- owlcms/src/main/frontend/components/DecisionBoard.js
+++ owlcms/src/main/frontend/components/DecisionBoard.js
@@ -13,6 +13,7 @@ class DecisionBoard extends LitElement {
   }
 
   render() {
+    const board = this.board;
     return html` 
       <link rel="stylesheet" type="text/css" .href="${stylesheetHref(this, "colors")}"/>
       <link rel="stylesheet" type="text/css" .href="${stylesheetHref(this, "decisionboard")}"/>
@@ -86,7 +87,7 @@ class DecisionBoard extends LitElement {
 	  </style>
     <div class="wrapper" style="${this.colorOverride}">
       <div class="wrapper bigTitle" style="${this.waitingStyles()}">
-        <div class="competitionName">${this.competitionName}</div>
+        <div class="competitionName">${board.competitionName}</div>
         <br />
         <div class="nextGroup">${this.t?.WaitingNextGroup}</div>
       </div>
@@ -114,28 +115,11 @@ class DecisionBoard extends LitElement {
 
   static get properties() {
     return {
-      // top
-      fullName: {},
-      weight: {},
-      attempt: {},
-      teamName: {},
-      startNumber: {},
-      decisionVisible: { type: Boolean },
-      competitionName: {},
-      groupName: {},
-      liftsDone: {},
+      boardState: { type: Object, noAccessor: true },
     
       athletes: {type: Object},
       leaders: {type: Object},
       records: {type: Object},
-
-      // mode (mutually exclusive, one of:
-      // WAIT INTRO_COUNTDOWN LIFT_COUNTDOWN CURRENT_ATHLETE INTERRUPTION SESSION_DONE CEREMONY
-      mode: {},
-
-      // during lifting
-      recordAttempt: {},
-      recordBroken: {},
 
       // style sheets & misc.
       javaComponentId: {},
@@ -152,80 +136,114 @@ class DecisionBoard extends LitElement {
     super.firstUpdated(_changedProperties);
   }
 
+  get board() {
+    return this._boardState;
+  }
+
+  get boardState() {
+    return this._boardState;
+  }
+
+  set boardState(value) {
+    const oldValue = this._boardState;
+    if (oldValue && value && Number(value.sequence) < Number(oldValue.sequence)) {
+      return;
+    }
+    this._boardState = value ?? DecisionBoard.emptyBoardState();
+    this.requestUpdate("boardState", oldValue);
+  }
+
+  static emptyBoardState() {
+    return {
+      athleteImg: "",
+      attempt: "",
+      competitionName: "",
+      decisionVisible: false,
+      mode: "WAIT",
+      recordAttempt: false,
+      recordBroken: false,
+      sequence: 0,
+      startNumber: 0,
+      teamFlagImg: "",
+      teamName: "",
+      weight: ""
+    };
+  }
+
   isBreak() {
-    return this.mode === "INTERRUPTION" || this.mode === "INTRO_COUNTDOWN" || this.mode === "LIFT_COUNTDOWN" || this.mode === "LIFT_COUNTDOWN_CEREMONY" || this.mode === "SESSION_DONE" || this.mode === "CEREMONY"
+    return this.board.mode === "INTERRUPTION" || this.board.mode === "INTRO_COUNTDOWN" || this.board.mode === "LIFT_COUNTDOWN" || this.board.mode === "LIFT_COUNTDOWN_CEREMONY" || this.board.mode === "SESSION_DONE" || this.board.mode === "CEREMONY"
   }
 
   isCountdown() {
-    return  this.mode === "INTRO_COUNTDOWN" || this.mode === "LIFT_COUNTDOWN" || this.mode === "LIFT_COUNTDOWN_CEREMONY"
+    return this.board.mode === "INTRO_COUNTDOWN" || this.board.mode === "LIFT_COUNTDOWN" || this.board.mode === "LIFT_COUNTDOWN_CEREMONY"
   }
 
   athleteImgClasses() {
     var mainClass = "picture";
     return mainClass + 
-      (this.decisionVisible ?  " hideBecauseDecision" : "") +
-      ((this.recordAttempt || this.recordBroken) ? " hideBecauseRecord" : "");
+      (this.board.decisionVisible ?  " hideBecauseDecision" : "") +
+      ((this.board.recordAttempt || this.board.recordBroken) ? " hideBecauseRecord" : "");
   }
   teamFlagImgClasses() {
-    var mainClass = this.athleteImg ? "flagWithPicture" : "flag";
+    var mainClass = this.board.athleteImg ? "flagWithPicture" : "flag";
     return mainClass + 
-      (this.decisionVisible ?  " hideBecauseDecision" : "") +
-      ((this.recordAttempt || this.recordBroken) ? " hideBecauseRecord" : "");
+      (this.board.decisionVisible ?  " hideBecauseDecision" : "") +
+      ((this.board.recordAttempt || this.board.recordBroken) ? " hideBecauseRecord" : "");
   }
 
   waitingStyles() {
-    return "display: " + (this.mode === "WAIT" ? "grid" : "none");
+    return "display: " + (this.board.mode === "WAIT" ? "grid" : "none");
   }
 
   activeStyles() {
-    return "display: " + ((this.mode !== "WAIT" && this.mode !== "INTERRUPTION") ? "grid" : "none");
+    return "display: " + ((this.board.mode !== "WAIT" && this.board.mode !== "INTERRUPTION") ? "grid" : "none");
   }
 
   stopStyles() {
-    return "display: " + (this.mode === "INTERRUPTION" ? "flex" : "none");
+    return "display: " + (this.board.mode === "INTERRUPTION" ? "flex" : "none");
   }
 
   lastNameClasses() {
-    return (this.athleteImg ? "lastNameWithPicture" : "lastName");
+    return (this.board.athleteImg ? "lastNameWithPicture" : "lastName");
   }
   lastNameStyles() {
     return "display: grid";
   }
 
   firstNameClasses() {
-    return "display: " + (this.athleteImg ? "firstNameWithPicture" : "firstName");
+    return "display: " + (this.board.athleteImg ? "firstNameWithPicture" : "firstName");
   }
   firstNameStyles() {
     return "display: grid";
   }
 
   teamNameStyles() {
-    return "display: " + ((this.recordAttempt || this.recordBroken || this.isBreak()) ? "none" : "grid");
+    return "display: " + ((this.board.recordAttempt || this.board.recordBroken || this.isBreak()) ? "none" : "grid");
   }
 
   teamFlagImgStyles() {
-    return "display: " + (this.isBreak() ? "none" : ( this.mode === "CURRENT_ATHLETE" ? "grid" : "none"));
+    return "display: " + (this.isBreak() ? "none" : (this.board.mode === "CURRENT_ATHLETE" ? "grid" : "none"));
   }
 
 
   athleteImgStyles() {
-    return "display: " + ((this.mode === "CURRENT_ATHLETE" && (this.recordAttempt || this.recordBroken)) ? "grid" : "none");
+    return "display: " + ((this.board.mode === "CURRENT_ATHLETE" && (this.board.recordAttempt || this.board.recordBroken)) ? "grid" : "none");
   }
 
   recordMessageClasses() {
     var mainClass = "recordNotification";
     return mainClass +
-      (this.recordAttempt ? " attempt" : "") +
-      (this.recordBroken ? " new" : "") + 
-      (!this.recordAttempt && !this.recordBroken ? " none" : "");
+      (this.board.recordAttempt ? " attempt" : "") +
+      (this.board.recordBroken ? " new" : "") +
+      (!this.board.recordAttempt && !this.board.recordBroken ? " none" : "");
   }
 
   recordMessageStyles() {
-    return "display: " + ((this.mode === "CURRENT_ATHLETE" && (this.recordAttempt || this.recordBroken)) ? "grid" : "none");
+    return "display: " + ((this.board.mode === "CURRENT_ATHLETE" && (this.board.recordAttempt || this.board.recordBroken)) ? "grid" : "none");
   }
 
   attemptStyles() {
-    return "display: " + ((this.isBreak() || this.decisionVisible) ? "none" : "grid");
+    return "display: " + ((this.isBreak() || this.board.decisionVisible) ? "none" : "grid");
   }
 
   startNumberStyles() {
@@ -234,15 +252,15 @@ class DecisionBoard extends LitElement {
 
   weightStyles() {
     // weights are visible during lift countdowns
-    return "display: " + ((this.mode === "LIFT_COUNTDOWN" || (this.mode === "CURRENT_ATHLETE")) ? "grid" : "none");
+    return "display: " + ((this.board.mode === "LIFT_COUNTDOWN" || this.board.mode === "CURRENT_ATHLETE") ? "grid" : "none");
   }
 
   athleteTimerStyles() {
-    return "display:" + ((this.mode === "CURRENT_ATHLETE" && !this.decisionVisible) ? "grid" : "none");
+    return "display:" + ((this.board.mode === "CURRENT_ATHLETE" && !this.board.decisionVisible) ? "grid" : "none");
   }
 
   breakTimerStyles() {
-    return "display:" + ((this.mode === "INTRO_COUNTDOWN" || this.mode === "LIFT_COUNTDOWN" || this.mode === "LIFT_COUNTDOWN_CEREMONY") ? "grid" : "none");
+    return "display:" + ((this.board.mode === "INTRO_COUNTDOWN" || this.board.mode === "LIFT_COUNTDOWN" || this.board.mode === "LIFT_COUNTDOWN_CEREMONY") ? "grid" : "none");
   }
 
   barbellStyles() {
@@ -250,27 +268,13 @@ class DecisionBoard extends LitElement {
   }
 
   decisionStyles() {
-    return "display: " + ((this.mode === "CURRENT_ATHLETE" && this.decisionVisible) ? "grid" : "none");
+    return "display: " + ((this.board.mode === "CURRENT_ATHLETE" && this.board.decisionVisible) ? "grid" : "none");
   }
 
   constructor() {
     super();
     this.javaComponentId = "";
-    this.lastName = "";
-    this.firstName = "";
-    this.weight = 0;
-    this.competitionName = "";
-
-    this.mode == "WAIT";
-
-    this.attempt = "";
-    this.athleteImg = "";
-    this.teamName = "";
-    this.teamFlagImg = "";
-    this.startNumber = 0;
-    this.decisionVisible = false;
-    this.recordAttempt = false;
-    this.recordBroken = false;
+    this._boardState = DecisionBoard.emptyBoardState();
 
     this.stylesDir = "";
     this.autoVersion = 0;
