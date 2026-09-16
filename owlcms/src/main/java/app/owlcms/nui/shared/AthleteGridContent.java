ab29114e3cd4b007d1c79c3a0b5b9725af2ff2d6
Jean-François Lamy <jf@jflamy.dev>
2026-09-16 15:22:01 -0400

    Announcer menu entries for Down and Timer sounds are operational again

--- owlcms/src/main/java/app/owlcms/nui/shared/AthleteGridContent.java
+++ owlcms/src/main/java/app/owlcms/nui/shared/AthleteGridContent.java
@@ -1831,7 +1831,11 @@ public abstract class AthleteGridContent extends BaseContent
 			return;
 		}
 		FieldOfPlay fop = getFop();
-		this.passiveTimer.setSilent(this.isSilenced() || (fop != null && fop.isEmitSoundsOnServer()));
+		this.passiveTimer.setSilent(isPassiveTimerSoundSilenced(fop != null && fop.isEmitSoundsOnServer()));
+	}
+
+	protected boolean isPassiveTimerSoundSilenced(boolean serverSound) {
+		return this.isSilenced() || serverSound;
 	}
 
 	private void passiveTimerApplyState(boolean running, Integer milliseconds) {
@@ -1858,7 +1862,7 @@ public abstract class AthleteGridContent extends BaseContent
 		if (this.passiveTimer == null) {
 			return;
 		}
-		this.passiveTimer.setSilent(this.isSilenced() || serverSound);
+		this.passiveTimer.setSilent(isPassiveTimerSoundSilenced(serverSound));
 		this.passiveTimer.start(milliseconds, issuedAtMillis);
 	}
 
